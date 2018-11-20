3dad37cbf8e9f1d5516e85f4cf275d344234cc00
Richard Hayes <richardhayes777@gmail.com>
2018-11-20 11:39:03 +0000

    removed irrelevant exceptions

--- autofit/exc.py
+++ autofit/exc.py
@@ -1,44 +1,8 @@
-class ImagingException(Exception):
-    pass
-
-
-class KernelException(Exception):
-    pass
-
-
 class PriorException(Exception):
     pass
 
 
 class MultiNestException(Exception):
-    pass
-
-
-class MaskException(Exception):
-    pass
-
-
-class GalaxyException(Exception):
-    pass
-
-
-class RayTracingException(Exception):
-    pass
-
-
-class PixelizationException(Exception):
-    pass
-
-
-class InversionException(Exception):
-    pass
-
-
-class FittingException(Exception):
-    pass
-
-
-class PlottingException(Exception):
     pass
 
 
