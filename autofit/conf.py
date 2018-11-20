ca7afb082512522319b70d6b992a82a5755c3ab8
Richard Hayes <richardhayes777@gmail.com>
2018-11-20 12:15:04 +0000

    modified configuration defaults

--- autofit/conf.py
+++ autofit/conf.py
@@ -265,20 +265,20 @@ The search is performed in this order:
 1) workspace. This is assumed to be in the same directory as autolens in the Docker container
 2) current working directory. This is to allow for installation and use with pip where users would expect the
    configuration in their current directory to be used.
-3) autolens. This is a backup for when no configuration is found. In this case it is still assumed a workspace directory
-   exists in the same directory as autolens.
+3) relative. This is a backup for when no configuration is found. In this case it is still assumed a workspace directory
+   exists in the same directory as autofit.
 """
 
 autofit_directory = os.path.dirname(os.path.realpath(__file__))
-workspace_directory = "{}/../workspace".format(autofit_directory)
+docker_workspace_directory = "/home/user/workspace"
 current_directory = os.getcwd()
 
-if is_config_in(workspace_directory):
-    CONFIG_PATH = "{}/config".format(workspace_directory)
-    instance = Config(CONFIG_PATH, "{}/output/".format(workspace_directory))
+if is_config_in(docker_workspace_directory):
+    CONFIG_PATH = "{}/config".format(docker_workspace_directory)
+    instance = Config(CONFIG_PATH, "{}/output/".format(docker_workspace_directory))
 elif is_config_in(current_directory):
     CONFIG_PATH = "{}/config".format(current_directory)
     instance = Config(CONFIG_PATH, "{}/output/".format(current_directory))
 else:
-    CONFIG_PATH = "{}/config".format(autofit_directory)
+    CONFIG_PATH = "{}/../workspace/config".format(autofit_directory)
     instance = Config(CONFIG_PATH, "{}/../workspace/output/".format(autofit_directory))
