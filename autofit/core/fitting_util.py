a0281e166501e644059e98ec52b178a376288173
James Nightingale <james.w.nightingale@durham.ac.uk>
2018-12-23 12:50:44 +0000

    Added asarray to masks so that inhrited masks can be used

--- autofit/core/fitting_util.py
+++ autofit/core/fitting_util.py
@@ -14,7 +14,7 @@ def residual_map_from_data_mask_and_model_data(data, mask, model_data):
     model_data : np.ndarray
         The model data used to fit the observed data.
     """
-    return np.subtract(data, model_data, out=np.zeros_like(data), where=mask==0)
+    return np.subtract(data, model_data, out=np.zeros_like(data), where=np.asarray(mask)==0)
 
 def chi_squared_map_from_residual_map_noise_map_and_mask(residual_map, noise_map, mask):
     """Computes the chi-squared map between a masked residual-map and noise-map, where:
@@ -33,7 +33,8 @@ def chi_squared_map_from_residual_map_noise_map_and_mask(residual_map, noise_map
     mask : np.ndarray
         The mask applied to the residual-map, where *False* entries are included in the calculation.
     """
-    return np.square(np.divide(residual_map, noise_map, out=np.zeros_like(residual_map), where=mask==0))
+    return np.square(np.divide(residual_map, noise_map, out=np.zeros_like(residual_map),
+                               where=np.asarray(mask)==0))
 
 def chi_squared_from_chi_squared_map_and_mask(chi_squared_map, mask):
     """Compute the chi-squared terms of each model's data-set's fit to an observed data-set, by summing the masked
@@ -46,7 +47,7 @@ def chi_squared_from_chi_squared_map_and_mask(chi_squared_map, mask):
     mask : np.ndarray
         The mask applied to the chi-squared map, where *False* entries are included in the calculation.
     """
-    return np.sum(chi_squared_map[mask==0])
+    return np.sum(chi_squared_map[np.asarray(mask)==0])
 
 def noise_normalization_from_noise_map_and_mask(noise_map, mask):
     """Compute the noise-map normalization terms of a list of masked 1D noise-maps, summing the noise_map vale in every
@@ -61,7 +62,7 @@ def noise_normalization_from_noise_map_and_mask(noise_map, mask):
     mask : np.ndarray
         The mask applied to the noise-map, where *False* entries are included in the calculation.
     """
-    return np.sum(np.log(2 * np.pi * noise_map[mask==0] ** 2.0))
+    return np.sum(np.log(2 * np.pi * noise_map[np.asarray(mask)==0] ** 2.0))
 
 def likelihood_from_chi_squared_and_noise_normalization(chi_squared, noise_normalization):
     """Compute the likelihood of each masked 1D model-data fit to the data, where:
--- test/autofit/test_fitting_util.py
+++ test/autofit/test_fitting_util.py
@@ -186,3 +186,29 @@ class TestLikelihood:
         noise_normalization = np.log(2 * np.pi * (2.0**2.0)) + np.log(2 * np.pi * (3.0**2.0))
 
         assert likelihood == pytest.approx(-0.5 * (chi_squared + noise_normalization), 1e-4)
+
+    def test__model_data_mismatch__mask_certain_pixels__lh_non_0__2d_data(self):
+
+        data = np.array([[10.0, 10.0],
+                         [10.0, 10.0]])
+        mask = np.array([[True, False],
+                         [False, True]])
+        noise_map = np.array([[1.0, 2.0],
+                              [3.0, 4.0]])
+        model_data = np.array([[11.0, 10.0],
+                                [9.0, 8.0]])
+
+        residual_map = fitting_util.residual_map_from_data_mask_and_model_data(data=data, mask=mask, model_data=model_data)
+        chi_squared_map = fitting_util.chi_squared_map_from_residual_map_noise_map_and_mask(residual_map=residual_map, mask=mask,
+                                                                                            noise_map=noise_map)
+        chi_squared = fitting_util.chi_squared_from_chi_squared_map_and_mask(mask=mask, chi_squared_map=chi_squared_map)
+        noise_normalization = fitting_util.noise_normalization_from_noise_map_and_mask(mask=mask, noise_map=noise_map)
+        likelihood = fitting_util.likelihood_from_chi_squared_and_noise_normalization(chi_squared=chi_squared,
+                                                                                      noise_normalization=noise_normalization)
+
+        # chi squared = 0, 0.25, (0.25 and 1.0 are masked)
+
+        chi_squared = 0.0 + (1.0 / 3.0)**2.0
+        noise_normalization = np.log(2 * np.pi * (2.0**2.0)) + np.log(2 * np.pi * (3.0**2.0))
+
+        assert likelihood == pytest.approx(-0.5 * (chi_squared + noise_normalization), 1e-4)
