d46972970ef03bfb063d140d84a6cd16d20a9e30
Richard Hayes <richardhayes777@gmail.com>
2019-01-10 10:35:53 +0000

    triangulation

--- test/autofit/test_grid_search.py
+++ test/autofit/test_grid_search.py
@@ -1,5 +1,6 @@
+import pytest
+
 from autofit.core import non_linear
-from autofit.core import prior as p
 from autofit.core.optimizer import grid
 
 
@@ -17,12 +18,21 @@ class MockAnalysis(non_linear.Analysis):
         pass
 
 
+def tuple_lists_equal(l1, l2):
+    for tuple_pair in zip(l1, l2):
+        for item in zip(tuple_pair[0], tuple_pair[1]):
+            if pytest.approx(item[0]) != pytest.approx(item[1]):
+                return False
+    return True
+
+
 class TestGridSearchOptimizer(object):
     def test_1d(self):
         points = []
         grid(lambda x: points.append(x), 1, 0.1)
         assert 10 == len(points)
-
+        assert tuple_lists_equal([(0.0,), (0.1,), (0.2,), (0.3,), (0.4,), (0.5,), (0.6,), (0.7,), (0.8,), (0.9,)],
+                                 points)
 
 # class TestGridSearch(object):
 #     def test_1d(self):
