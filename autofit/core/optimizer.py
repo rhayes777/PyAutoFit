71e1a5f56897e74967e6c57b5ca9f2a662489b5b
Richard Hayes <richardhayes777@gmail.com>
2019-01-10 11:08:17 +0000

    N dimensional

--- autofit/core/optimizer.py
+++ autofit/core/optimizer.py
@@ -1,18 +1,11 @@
-# (0, 0), (0, 1), (1, 0), (1, 1)
-
-
 def grid(fitness_function, no_dimensions, step_size):
-    for value in range(0, int(1 / step_size)):
-        fitness_function((step_size * value,))
+    for arguments in make_lists(no_dimensions, step_size):
+        fitness_function(arguments)
 
 
 def make_lists(no_dimensions, step_size):
     if no_dimensions == 0:
         return [[]]
 
-    sub_lists = list(make_lists(no_dimensions - 1, step_size))
+    sub_lists = make_lists(no_dimensions - 1, step_size)
     return [[step_size * value] + sub_list for value in range(0, int(1 / step_size)) for sub_list in sub_lists]
-
-
-if __name__ == "__main__":
-    print(make_lists(2, 0.2))
