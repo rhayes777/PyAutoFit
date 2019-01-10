e223e8ec88237e5cdcf9b25034883596d605f8cd
Richard Hayes <richardhayes777@gmail.com>
2019-01-10 11:44:22 +0000

    basic non_linear grid search test passing

--- test/autofit/test_grid_search.py
+++ test/autofit/test_grid_search.py
@@ -1,5 +1,6 @@
 import pytest
 
+from autofit import mock
 from autofit.core import non_linear
 from autofit.core.optimizer import grid
 
@@ -83,10 +84,13 @@ class TestGridSearchOptimizer(object):
 
         assert result == best_point
 
-# class TestGridSearch(object):
-#     def test_1d(self):
-#         grid_search = non_linear.GridSearch(step_size=0.1)
-#         grid_search.variable.one = p.UniformPrior()
-#         analysis = MockAnalysis()
-#         grid_search.fit(analysis)
-#         assert len(analysis.instances) == 10
+
+class TestGridSearch(object):
+    def test_1d(self):
+        grid_search = non_linear.GridSearch(step_size=0.1)
+        grid_search.variable.one = mock.Galaxy
+
+        analysis = MockAnalysis()
+        grid_search.fit(analysis)
+
+        assert len(analysis.instances) == 11
