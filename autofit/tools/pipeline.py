5092575e8e4c3e52035e77c43c1380a487cbda1a
Richard Hayes <richardhayes777@gmail.com>
2019-02-26 17:32:20 +0000

    docs

--- autofit/tools/pipeline.py
+++ autofit/tools/pipeline.py
@@ -7,22 +7,42 @@ logger = logging.getLogger(__name__)
 
 class ResultsCollection(object):
     def __init__(self):
+        """
+        A collection of results from previous phases. Results can be obtained using an index or the name of the phase
+        from whence they came.
+        """
         self.__result_list = []
         self.__result_dict = {}
 
     @property
     def last(self):
+        """
+        The result of the last phase
+        """
         if len(self.__result_list) > 0:
             return self.__result_list[-1]
         return None
 
     @property
     def first(self):
+        """
+        The result of the first phase
+        """
         if len(self.__result_list) > 0:
             return self.__result_list[0]
         return None
 
     def add(self, phase_name, result):
+        """
+        Add the result of a phase.
+
+        Parameters
+        ----------
+        phase_name: str
+            The name of the phase
+        result
+            The result of that phase
+        """
         if phase_name in self.__result_dict:
             raise exc.PipelineException(
                 "Results from a phase called {} already exist in the pipeline".format(phase_name))
@@ -30,12 +50,43 @@ class ResultsCollection(object):
         self.__result_dict[phase_name] = result
 
     def __getitem__(self, item):
+        """
+        Get the result of a previous phase by index
+
+        Parameters
+        ----------
+        item: int
+            The index of the result
+
+        Returns
+        -------
+        result: Result
+            The result of a previous phase
+        """
         return self.__result_list[item]
 
     def __len__(self):
         return len(self.__result_dict)
 
     def from_phase(self, phase_name):
+        """
+        Returns the result of a previous phase by its name
+
+        Parameters
+        ----------
+        phase_name: str
+            The name of a previous phase
+
+        Returns
+        -------
+        result: Result
+            The result of that phase
+
+        Raises
+        ------
+        exc.PipelineException
+            If no phase with the expected result is found
+        """
         try:
             return self.__result_dict[phase_name]
         except KeyError:
@@ -47,6 +98,7 @@ class Pipeline(object):
 
     def __init__(self, pipeline_name, *phases):
         """
+        A pipeline of phases to be run sequentially. Results are passed between phases. Phases must have unique names.
 
         Parameters
         ----------
@@ -77,6 +129,19 @@ class Pipeline(object):
         return self.__class__("{} + {}".format(self.pipeline_name, other.pipeline_name), *(self.phases + other.phases))
 
     def run_function(self, func):
+        """
+        Run the function for each phase in the pipeline.
+
+        Parameters
+        ----------
+        func
+            A function that takes a phase and prior results, returning results for that phase
+
+        Returns
+        -------
+        results: ResultsCollection
+            A collection of results
+        """
         results = ResultsCollection()
         for i, phase in enumerate(self.phases):
             logger.info("Running Phase {} (Number {})".format(phase.optimizer.name, i))
