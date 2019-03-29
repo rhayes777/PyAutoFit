cdd3e2ad32f752b7476d008c546e86817d70979e
Richard Hayes <richardhayes777@gmail.com>
2019-03-29 13:54:42 +0000

    demonstrating phase path persistence

--- test/optimize/test_persistance.py
+++ test/optimize/test_persistance.py
@@ -1,11 +1,14 @@
+import pickle
+
 from autofit import mock
 from autofit.optimize import non_linear as nl
-import pickle
 
 
 class TestCase(object):
     def test_simple_pickle(self):
         optimiser = nl.MultiNest("phasename")
         optimiser.variable.profile = mock.EllipticalProfile
+        pickled_optimiser = pickle.loads(pickle.dumps(optimiser))
 
-        assert optimiser.variable.priors == pickle.loads(pickle.dumps(optimiser)).variable.priors
+        assert optimiser.variable.priors == pickled_optimiser.variable.priors
+        assert optimiser.phase_path == pickled_optimiser.phase_path
