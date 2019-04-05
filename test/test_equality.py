9b6fbc9fac3167853f7de620d2a94ec3dbe3285a
Richard Hayes <richardhayes777@gmail.com>
2019-04-05 08:26:24 +0100

    test against equality of list prior models

--- test/test_equality.py
+++ test/test_equality.py
@@ -14,3 +14,12 @@ class TestCase(object):
         prior_model_copy.centre_0 = p.UniformPrior()
 
         assert prior_model != prior_model_copy
+
+    def test_list_prior_model(self):
+        list_prior_model = pm.ListPriorModel([pm.PriorModel(mock.GeometryProfile)])
+        list_prior_model_copy = deepcopy(list_prior_model)
+        assert list_prior_model == list_prior_model_copy
+
+        list_prior_model[0].centre_0 = p.UniformPrior()
+
+        assert list_prior_model != list_prior_model_copy
