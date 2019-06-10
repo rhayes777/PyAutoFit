716638dd77da708b05e55968b5b8e51b1fc46f5a
Richard Hayes <richardhayes777@gmail.com>
2019-06-10 16:10:21 +0100

    imports and format

--- test/mapper/test_model_instance.py
+++ test/mapper/test_model_instance.py
@@ -1,55 +1,58 @@
-import autofit.mapper
-import test.mock
+from autofit.mapper import model
 from autofit.mapper import model_mapper, prior as p
+from test import mock
 from test.mapper.test_model_mapper import MockClassMM, MockProfile
 
 
 class TestModelInstance(object):
     def test_instances_of(self):
-        instance = autofit.mapper.model.ModelInstance()
-        instance.galaxy_1 = test.mock.Galaxy()
-        instance.galaxy_2 = test.mock.Galaxy()
-        assert instance.instances_of(test.mock.Galaxy) == [instance.galaxy_1, instance.galaxy_2]
+        instance = model.ModelInstance()
+        instance.galaxy_1 = mock.Galaxy()
+        instance.galaxy_2 = mock.Galaxy()
+        assert instance.instances_of(mock.Galaxy) == [instance.galaxy_1,
+                                                      instance.galaxy_2]
 
     def test_name_instance_tuples_for_class(self):
-        sub = autofit.mapper.model.ModelInstance()
-        instance = autofit.mapper.model.ModelInstance()
+        sub = model.ModelInstance()
+        instance = model.ModelInstance()
 
-        galaxy_1 = test.mock.Galaxy()
+        galaxy_1 = mock.Galaxy()
         sub.galaxy_1 = galaxy_1
 
-        galaxy_2 = test.mock.Galaxy()
+        galaxy_2 = mock.Galaxy()
         instance.galaxy_2 = galaxy_2
         instance.sub = sub
 
-        result = instance.name_instance_tuples_for_class(test.mock.Galaxy)
+        result = instance.name_instance_tuples_for_class(mock.Galaxy)
 
         assert result[0] == ("galaxy_2", galaxy_2)
         assert result[1] == ("sub_galaxy_1", galaxy_1)
 
     def test_instances_of_filtering(self):
-        instance = autofit.mapper.model.ModelInstance()
-        instance.galaxy_1 = test.mock.Galaxy()
-        instance.galaxy_2 = test.mock.Galaxy()
-        instance.other = test.mock.GalaxyModel()
-        assert instance.instances_of(test.mock.Galaxy) == [instance.galaxy_1, instance.galaxy_2]
+        instance = model.ModelInstance()
+        instance.galaxy_1 = mock.Galaxy()
+        instance.galaxy_2 = mock.Galaxy()
+        instance.other = mock.GalaxyModel()
+        assert instance.instances_of(mock.Galaxy) == [instance.galaxy_1,
+                                                      instance.galaxy_2]
 
     def test_instances_from_list(self):
-        instance = autofit.mapper.model.ModelInstance()
-        galaxy_1 = test.mock.Galaxy()
-        galaxy_2 = test.mock.Galaxy()
+        instance = model.ModelInstance()
+        galaxy_1 = mock.Galaxy()
+        galaxy_2 = mock.Galaxy()
         instance.galaxies = [galaxy_1, galaxy_2]
-        assert instance.instances_of(test.mock.Galaxy) == [galaxy_1, galaxy_2]
+        assert instance.instances_of(mock.Galaxy) == [galaxy_1, galaxy_2]
 
     def test_non_trivial_instances_of(self):
-        instance = autofit.mapper.model.ModelInstance()
-        galaxy_1 = test.mock.Galaxy(redshift=1)
-        galaxy_2 = test.mock.Galaxy(redshift=2)
-        instance.galaxies = [galaxy_1, galaxy_2, test.mock.GalaxyModel]
-        instance.galaxy_3 = test.mock.Galaxy(redshift=3)
-        instance.galaxy_prior = test.mock.GalaxyModel()
+        instance = model.ModelInstance()
+        galaxy_1 = mock.Galaxy(redshift=1)
+        galaxy_2 = mock.Galaxy(redshift=2)
+        instance.galaxies = [galaxy_1, galaxy_2, mock.GalaxyModel]
+        instance.galaxy_3 = mock.Galaxy(redshift=3)
+        instance.galaxy_prior = mock.GalaxyModel()
 
-        assert instance.instances_of(test.mock.Galaxy) == [instance.galaxy_3, galaxy_1, galaxy_2]
+        assert instance.instances_of(mock.Galaxy) == [instance.galaxy_3, galaxy_1,
+                                                      galaxy_2]
 
     def test_simple_model(self):
         mapper = model_mapper.ModelMapper()
