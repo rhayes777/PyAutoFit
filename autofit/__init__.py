476d57bf52aaa92659863e3716700aa2df2c6c2b
Richard Hayes <richardhayes777@gmail.com>
2019-06-24 15:23:44 +0100

    simplified imports

--- autofit/__init__.py
+++ autofit/__init__.py
@@ -1,10 +1,6 @@
 from .mapper import *
 from .tools import *
-from autofit.optimize.non_linear.downhill_simplex import DownhillSimplex
-from autofit.optimize.non_linear.multi_nest import MultiNest
-from autofit.optimize.non_linear.non_linear import Analysis
-from autofit.optimize.non_linear.non_linear import NonLinearOptimizer
-from autofit.optimize.non_linear.non_linear import Result
+from .optimize import *
 from . import exc
 from . import conf
 
---
+++ autofit/optimize/__init__.py
@@ -0,0 +1,5 @@
+from .non_linear.downhill_simplex import DownhillSimplex
+from .non_linear.multi_nest import MultiNest
+from .non_linear.non_linear import Analysis
+from .non_linear.non_linear import NonLinearOptimizer
+from .non_linear.non_linear import Result
