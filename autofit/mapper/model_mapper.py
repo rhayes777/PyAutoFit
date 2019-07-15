15230d2a3797a23ff4e899acf587af94438708f9
Richard Hayes <richardhayes777@gmail.com>
2019-07-15 16:18:04 +0100

    reformated model mapper imports

--- autofit/mapper/model_mapper.py
+++ autofit/mapper/model_mapper.py
@@ -4,10 +4,10 @@ import os
 from autofit import conf
 from autofit import exc
 from autofit.mapper.model import ModelInstance
-from autofit.mapper.prior_model.prior import GaussianPrior, cast_collection, PriorNameValue, \
-    ConstantNameValue, Prior
 from autofit.mapper.prior_model import AbstractPriorModel
 from autofit.mapper.prior_model import CollectionPriorModel
+from autofit.mapper.prior_model.prior import GaussianPrior, cast_collection, PriorNameValue, \
+    ConstantNameValue, Prior
 from autofit.mapper.prior_model.util import PriorModelNameValue
 
 path = os.path.dirname(os.path.realpath(__file__))
