c667fba7befa47b43f0240e901742fa837d3cc2a
Richard Hayes <richardhayes777@gmail.com>
2019-07-15 11:52:11 +0100

    format

--- test/conftest.py
+++ test/conftest.py
@@ -10,4 +10,4 @@ directory = path.dirname(path.realpath(__file__))
 @pytest.fixture(autouse=True)
 def set_config_path():
     af.conf.instance = af.conf.Config(path.join(directory, "test_files/config"),
-                                path.join(directory, "output"))
+                                      path.join(directory, "output"))
