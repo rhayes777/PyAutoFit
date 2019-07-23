1ed098366de591ada6958b73de4aeec82aac96c8
Richard Hayes <richardhayes777@gmail.com>
2019-07-23 17:03:13 +0100

    extracted fixtures

--- test/mapper/test_prior_model.py
+++ test/mapper/test_prior_model.py
@@ -5,30 +5,38 @@ from test.mock import SimpleClass, ComplexClass, ListClass, Distance, \
     DistanceClass, PositionClass, Galaxy, Tracer, EllipticalLP, EllipticalMassProfile
 
 
-class TestFromInstance:
-    def test_prior_model_from_instance(self):
-        instance = SimpleClass(1.0, 2.0)
-        prior_model = af.AbstractPriorModel.from_instance(
-            instance
-        )
-        assert prior_model.cls == SimpleClass
-        assert prior_model.prior_count == 0
-        assert prior_model.one == 1.0
-        assert prior_model.two == 2.0
+@pytest.fixture(name="instance_prior_model")
+def make_instance_prior_model():
+    instance = SimpleClass(1.0, 2.0)
+    return af.AbstractPriorModel.from_instance(
+        instance
+    )
 
-        new_instance = prior_model.instance_for_arguments({})
+
+@pytest.fixture(name="list_prior_model")
+def make_list_prior_model():
+    instance = [SimpleClass(1.0, 2.0)]
+    return af.AbstractPriorModel.from_instance(
+        instance
+    )
+
+
+class TestFromInstance:
+    def test_instance(self, instance_prior_model):
+        assert instance_prior_model.cls == SimpleClass
+        assert instance_prior_model.prior_count == 0
+        assert instance_prior_model.one == 1.0
+        assert instance_prior_model.two == 2.0
+
+        new_instance = instance_prior_model.instance_for_arguments({})
         assert isinstance(new_instance, SimpleClass)
         assert new_instance.one == 1.0
         assert new_instance.two == 2.0
 
-    def test_list(self):
-        instance = [SimpleClass(1.0, 2.0)]
-        prior_model = af.AbstractPriorModel.from_instance(
-            instance
-        )
-        assert isinstance(prior_model, af.CollectionPriorModel)
-        assert isinstance(prior_model[0], af.PriorModel)
-        assert prior_model[0].one == 1.0
+    def test_list(self, list_prior_model):
+        assert isinstance(list_prior_model, af.CollectionPriorModel)
+        assert isinstance(list_prior_model[0], af.PriorModel)
+        assert list_prior_model[0].one == 1.0
 
     def test_dict(self):
         instance = {
