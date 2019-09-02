e5eb5e30141c4ab1580f9390b46abdb24a2a9f10
Richard Hayes <richardhayes777@gmail.com>
2019-09-02 14:24:58 +0100

    working result paths

--- autofit/tools/promise.py
+++ autofit/tools/promise.py
@@ -1,17 +1,33 @@
 class PromiseResult:
-    def __init__(self, phase):
+    def __init__(self, phase, *result_path, assert_exists=True):
         self.phase = phase
+        self.assert_exists = assert_exists
+        self.result_path = result_path
 
     @property
     def variable(self):
-        return Promise(self.phase)
+        return Promise(
+            self.phase,
+            result_path=self.result_path,
+            assert_exists=self.assert_exists
+        )
 
     @property
     def constant(self):
-        return Promise(self.phase, is_constant=True)
+        return Promise(
+            self.phase,
+            result_path=self.result_path,
+            is_constant=True,
+            assert_exists=self.assert_exists
+        )
 
     def __getattr__(self, item):
-        return PromiseResult(phase=self.phase)
+        return PromiseResult(
+            self.phase,
+            *self.result_path,
+            item,
+            assert_exists=False
+        )
 
 
 class Promise:
@@ -19,12 +35,17 @@ class Promise:
             self,
             phase,
             *path,
-            is_constant=False
+            result_path,
+            is_constant=False,
+            assert_exists=True
     ):
         self.phase = phase
         self.path = path
         self.is_constant = is_constant
-        phase.variable.object_for_path(path)
+        self.assert_exists = assert_exists
+        self.result_path = result_path
+        if assert_exists:
+            phase.variable.object_for_path(path)
 
     def __getattr__(self, item):
         if item in ("phase", "path", "is_constant"):
@@ -33,10 +54,14 @@ class Promise:
             self.phase,
             *self.path,
             item,
-            is_constant=self.is_constant
+            result_path=self.result_path,
+            is_constant=self.is_constant,
+            assert_exists=self.assert_exists
         )
 
     def populate(self, results_collection):
         results = results_collection.from_phase(self.phase.phase_name)
+        for item in self.result_path:
+            results = getattr(results, item)
         model = results.constant if self.is_constant else results.variable
         return model.object_for_path(self.path)
