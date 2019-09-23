808e0db9bcfd436efa3f43a761856ea21735c0f7
Richard Hayes <richardhayes777@gmail.com>
2019-09-23 10:20:55 +0100

    docs

--- autofit/tools/promise.py
+++ autofit/tools/promise.py
@@ -1,11 +1,28 @@
 class PromiseResult:
     def __init__(self, phase, *result_path, assert_exists=True):
+        """
+        A wrapper for a phase that facilitates the generation of priors by consuming user defined paths.
+
+        Parameters
+        ----------
+        phase
+            A phase from which a promise is generated
+        result_path
+            The path through the results objects to the correct result. This is used for phase extensions when
+            a result object can have child result objects
+        assert_exists
+            If this is true then an AttributeError is thrown if there is no object with the given path in the
+            model of the origin phase
+        """
         self.phase = phase
         self.assert_exists = assert_exists
         self.result_path = result_path
 
     @property
     def variable(self):
+        """
+        A promise for an object in the variable result. This might be a prior or prior model.
+        """
         return Promise(
             self.phase,
             result_path=self.result_path,
@@ -14,6 +31,9 @@ class PromiseResult:
 
     @property
     def constant(self):
+        """
+        A promise for an object in the best fit result. This must be an instance or constant.
+        """
         return Promise(
             self.phase,
             result_path=self.result_path,
@@ -39,6 +59,26 @@ class Promise:
             is_constant=False,
             assert_exists=True
     ):
+        """
+        Place holder for an object in the object hierarchy. This is replaced at runtime by a prior, prior
+        model, constant or instance
+
+        Parameters
+        ----------
+        phase
+            The phase that holds or creates the promised object
+        path
+            The path to the promised object. e.g. if a phase has a variable galaxies.lens.phi then the path
+            will be ("galaxies", "lens", "phi")
+        result_path
+            The path through the result collection to the result object required. This is used for hyper phases
+            where a result object can have child result objects for each phase extension.
+        is_constant
+            True if the promised object belongs to the constant result object
+        assert_exists
+            If this is true then an exception is raised if an object is not defined in the addressed phase's
+            model. Hyper phases are a bit trickier so no assertion is made.
+        """
         self.phase = phase
         self.path = path
         self.is_constant = is_constant
@@ -63,6 +103,20 @@ class Promise:
         )
 
     def populate(self, results_collection):
+        """
+        Find the object that this promise corresponds to be getting results for a particular phase and then
+        traversing those results using the path.
+
+        Parameters
+        ----------
+        results_collection
+            A collection of results from previous phases
+
+        Returns
+        -------
+        obj
+            The promised prior, prior model, instance or constant
+        """
         results = results_collection.from_phase(self.phase.phase_name)
         for item in self.result_path:
             results = getattr(results, item)
