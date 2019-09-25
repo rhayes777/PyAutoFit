ee13b6e5f8d255b6f36dd9a839ce6a0b84ff3d6a
Richard Hayes <richardhayes777@gmail.com>
2019-09-25 14:06:59 +0100

    representation of phases

--- autofit/tools/phase.py
+++ autofit/tools/phase.py
@@ -39,6 +39,12 @@ class AbstractPhase:
         )
         self.auto_link_priors = auto_link_priors
 
+    def __str__(self):
+        return self.phase_name
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} {self.phase_name}>"
+
     @property
     def result(self):
         return PromiseResult(self)
