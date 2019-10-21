ceae797d4283b7df3d8c4c15d035a1e751cf8951
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-10-21 20:23:13 +0100

    Begun to add integration test functionality using automodel.

---
+++ test_autofit/integration/tests/runner.py
@@ -0,0 +1,55 @@
+import os
+import autoarray as aa
+import autofit as af
+from test_autofit.integration import integration_util
+from autofit.optimize.non_linear.mock_nlo import MockNLO
+
+
+def run(
+    module,
+    test_name=None,
+    optimizer_class=af.MultiNest,
+    config_folder="config",
+):
+    test_name = test_name or module.test_name
+    test_path = "{}/../".format(os.path.dirname(os.path.realpath(__file__)))
+    output_path = test_path + "output/"
+    config_path = test_path + config_folder
+    af.conf.instance = af.conf.Config(config_path=config_path, output_path=output_path)
+    integration_util.reset_paths(test_name=test_name, output_path=output_path)
+
+    data_path = af.path_util.make_and_return_path_from_path_and_folder_names(
+        path=test_path, folder_names=["data", module.data_type]
+    )
+
+    imaging = aa.imaging.from_fits(
+        image_path=data_path + "/image.fits",
+        noise_map_path=data_path + "/noise_map.fits",
+        pixel_scales=0.1,
+    )
+
+    module.make_pipeline(
+        name=test_name,
+        phase_folders=[module.test_type, test_name],
+        optimizer_class=optimizer_class,
+    ).run(data=imaging)
+
+
+def run_a_mock(module):
+    # noinspection PyTypeChecker
+    run(
+        module,
+        test_name=f"{module.test_name}_mock",
+        optimizer_class=MockNLO,
+        config_folder="config_mock",
+    )
+
+
+def run_with_multi_nest(module):
+    # noinspection PyTypeChecker
+    run(
+        module,
+        test_name=f"{module.test_name}_nest",
+        optimizer_class=af.MultiNest,
+        config_folder="config_mock",
+    )
---
+++ test_autofit/simulate/gaussian/data_maker.py
@@ -0,0 +1,11 @@
+from test_autofit.simulate.gaussian import makers
+
+# Welcome to the PyAutoFit test_autoarray suite data_type maker. Here, we'll make the suite of data_type that we use to test_autoarray and profile
+# PyAutoLens. This consists of the following sets of images:
+
+sub_size = 1
+
+# To simulate each lens, we pass it a name and call its maker. In the makers.py file, you'll see the
+makers.make__gaussian(
+    sub_size=sub_size
+)
