469cc8c26e8d13f6bb85438b846a14040a2f70bf
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-10-27 11:49:25 +0000

    combined different fit plotters into imaging_fit_plotter module with all tests now passing.

--- autofit/__init__.py
+++ autofit/__init__.py
@@ -27,6 +27,7 @@ from .mapper.prior_model.dimension_type import DimensionType, map_types
 from .mapper.prior_model.prior_model import PriorModel
 from .mapper.prior_model.util import PriorModelNameValue
 from .optimize import *
+from .optimize.non_linear.non_linear import Paths
 from .optimize.non_linear.downhill_simplex import DownhillSimplex
 from .optimize.non_linear.downhill_simplex import DownhillSimplex
 from .optimize.non_linear.grid_search import GridSearch
