1465f16469c08ba4ced2fe774e1f04f694965ae6
Richard <richardhayes777@gmail.com>
2019-11-11 09:48:27 +0000

    more testing

--- test_autofit/optimize/test_paths.py
+++ test_autofit/optimize/test_paths.py
@@ -2,17 +2,21 @@ import autofit as af
 
 
 class TestPathDecorator:
+    @staticmethod
+    def assert_paths_as_expected(
+            paths
+    ):
+        assert paths.phase_name == "phase_name"
+        assert paths.phase_tag == "phase_tag"
+        assert paths.phase_folders == ["phase_folders"]
+
     def test_with_arguments(self):
         optimizer = af.NonLinearOptimizer(
             phase_name="phase_name",
             phase_tag="phase_tag",
             phase_folders=("phase_folders",),
         )
-        paths = optimizer.paths
-
-        assert paths.phase_name == "phase_name"
-        assert paths.phase_tag == "phase_tag"
-        assert paths.phase_folders == ["phase_folders"]
+        self.assert_paths_as_expected(optimizer.paths)
 
     def test_positional(self):
         optimizer = af.NonLinearOptimizer(
@@ -30,8 +34,15 @@ class TestPathDecorator:
                 phase_folders=("phase_folders",),
             )
         )
-        paths = optimizer.paths
+        self.assert_paths_as_expected(optimizer.paths)
 
-        assert paths.phase_name == "phase_name"
-        assert paths.phase_tag == "phase_tag"
-        assert paths.phase_folders == ["phase_folders"]
+    def test_combination_argument(self):
+        optimizer = af.NonLinearOptimizer(
+            "other",
+            paths=af.Paths(
+                phase_name="phase_name",
+                phase_tag="phase_tag",
+                phase_folders=("phase_folders",),
+            )
+        )
+        self.assert_paths_as_expected(optimizer.paths)
