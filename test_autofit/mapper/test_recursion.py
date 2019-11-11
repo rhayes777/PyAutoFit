cf92df9ab61bf65c10b61caf0c943822e27b914a
Richard <richardhayes777@gmail.com>
2019-11-11 12:05:40 +0000

    handling recursive population in simple case

--- test_autofit/mapper/test_recursion.py
+++ test_autofit/mapper/test_recursion.py
@@ -6,6 +6,51 @@ class RecursionPromise:
 
 
 cache = {}
+result_cache = {}
+
+
+def replace_promise(
+        promise,
+        obj,
+        true_value
+):
+    if isinstance(
+            obj,
+            list
+    ):
+        return [
+            replace_promise(
+                promise,
+                item,
+                true_value
+            )
+            for item in obj
+        ]
+    if isinstance(
+            obj,
+            dict
+    ):
+        return {
+            key: replace_promise(
+                promise,
+                value,
+                true_value
+            )
+            for key, value
+            in obj.items()
+        }
+    if obj is promise:
+        return true_value
+    try:
+        for key, value in obj.__dict__.items():
+            setattr(obj, key, replace_promise(
+                promise,
+                value,
+                true_value
+            ))
+    except AttributeError:
+        pass
+    return obj
 
 
 def dynamic_recursion_cache(
@@ -16,10 +61,18 @@ def dynamic_recursion_cache(
         item_id = id(item)
         if item_id in cache:
             return cache[item_id]
+        recursion_promise = RecursionPromise()
         cache[
             item_id
-        ] = RecursionPromise()
-        return func(item)
+        ] = recursion_promise
+        result = func(item)
+        result = replace_promise(
+            recursion_promise,
+            result,
+            result
+        )
+        del cache[item_id]
+        return result
 
     return wrapper
 
