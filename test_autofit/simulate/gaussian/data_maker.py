a34784bad1285e8303aff6706212c21115667bce
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-11-18 09:14:23 +0000

    Sorting out visualization to make general for toy models.

--- test_autofit/simulate/gaussian/data_maker.py
+++
@@ -1,9 +0,0 @@
-from test_autofit.simulate.gaussian import makers
-
-# Welcome to the PyAutoFit test_autoarray suite data_type maker. Here, we'll make the suite of data_type that we use to test and profile
-# PyAutoLens. This consists of the following sets of images:
-
-sub_size = 1
-
-# To simulator each lens, we pass it a name and call its maker. In the makers.py file, you'll see the
-makers.make__gaussian(sub_size=sub_size)
