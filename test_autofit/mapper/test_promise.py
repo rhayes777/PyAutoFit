6566349963f277cbacf139dd6ba5c5125d4c786b
Richard <richardhayes777@gmail.com>
2019-12-04 09:18:56 +0000

    another test doemonstrating hyper -1 model

--- test_autofit/mapper/test_promise.py
+++ test_autofit/mapper/test_promise.py
@@ -73,6 +73,23 @@ class TestLastPromises:
             collection
         ) is result
 
+    def test_second_indexed_hyper(self, collection):
+        result = mock.Result(
+            model=af.ModelMapper(),
+            instance=af.ModelInstance()
+        )
+        collection.add(
+            "next",
+            result
+        )
+        result = af.last[-1].hyper_result.model.populate(
+            collection
+        )
+        assert isinstance(result, af.ModelMapper)
+        assert af.last.hyper_result[-1].model.populate(
+            collection
+        ) is result
+
     def test_model_absolute(self, collection):
         result = af.last.model_absolute(10).one.redshift.populate(collection)
 
