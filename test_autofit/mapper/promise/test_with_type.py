27c7d4aee72f578ac7f2d3717c6d24b7e76fcb3a
Richard <richardhayes777@gmail.com>
2020-02-03 17:41:11 +0000

    test for instance with type

--- test_autofit/mapper/promise/test_with_type.py
+++ test_autofit/mapper/promise/test_with_type.py
@@ -14,3 +14,16 @@ def test_model_with_type(
 
     assert isinstance(result, af.PriorModel)
     assert result.cls == mock.Galaxy
+
+
+def test_instance_with_type(
+        phase,
+        collection
+):
+    promise = phase.result.instance[
+        mock.Galaxy
+    ][0]
+
+    result = promise.populate(collection)
+
+    assert isinstance(result, mock.Galaxy)
