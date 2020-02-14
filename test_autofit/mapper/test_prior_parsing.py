00bed5e20991c1eb9c125a9d67e25b43004f1a8b
Richard <richardhayes777@gmail.com>
2020-02-14 10:55:08 +0000

    extracted fixtures

--- test_autofit/mapper/test_prior_parsing.py
+++ test_autofit/mapper/test_prior_parsing.py
@@ -1,47 +1,94 @@
+import pytest
+
 import autofit as af
 
 
-def test_uniform():
-    uniform_prior = af.Prior.from_dict(
-        {
-            "type": "Uniform",
-            "lower_limit": 2,
-            "upper_limit": 3
-        }
+@pytest.fixture(
+    name="uniform_dict"
+)
+def make_uniform_dict():
+    return {
+        "type": "Uniform",
+        "lower_limit": 2,
+        "upper_limit": 3
+    }
+
+
+@pytest.fixture(
+    name="uniform_prior"
+)
+def make_uniform_prior(
+        uniform_dict
+):
+    return af.Prior.from_dict(
+        uniform_dict
     )
 
-    assert isinstance(uniform_prior, af.UniformPrior)
-    assert uniform_prior.lower_limit == 2
-    assert uniform_prior.upper_limit == 3
+
+@pytest.fixture(
+    name="log_uniform_dict"
+)
+def make_log_uniform_dict():
+    return {
+        "type": "LogUniform",
+        "lower_limit": 0.2,
+        "upper_limit": 0.3
+    }
 
 
-def test_log_uniform():
-    log_uniform_prior = af.Prior.from_dict(
-        {
-            "type": "LogUniform",
-            "lower_limit": 0.2,
-            "upper_limit": 0.3
-        }
+@pytest.fixture(
+    name="log_uniform_prior"
+)
+def make_log_uniform_prior(
+        log_uniform_dict
+):
+    return af.Prior.from_dict(
+        log_uniform_dict
     )
 
-    assert isinstance(log_uniform_prior, af.LogUniformPrior)
-    assert log_uniform_prior.lower_limit == 0.2
-    assert log_uniform_prior.upper_limit == 0.3
+
+@pytest.fixture(
+    name="gaussian_dict"
+)
+def make_gaussian_dict():
+    return {
+        "type": "Gaussian",
+        "lower_limit": -10,
+        "upper_limit": 10,
+        "mean": 3,
+        "sigma": 4
+    }
 
 
-def test_gaussian():
-    gaussian_prior = af.GaussianPrior.from_dict(
-        {
-            "type": "Gaussian",
-            "lower_limit": -10,
-            "upper_limit": 10,
-            "mean": 3,
-            "sigma": 4
-        }
+@pytest.fixture(
+    name="gaussian_prior"
+)
+def make_gaussian_prior(
+        gaussian_dict
+):
+    return af.Prior.from_dict(
+        gaussian_dict
     )
 
-    assert isinstance(gaussian_prior, af.GaussianPrior)
-    assert gaussian_prior.lower_limit == -10
-    assert gaussian_prior.upper_limit == 10
-    assert gaussian_prior.mean == 3
-    assert gaussian_prior.sigma == 4
+
+class TestDict:
+    pass
+
+
+class TestFromDict:
+    def test_uniform(self, uniform_prior):
+        assert isinstance(uniform_prior, af.UniformPrior)
+        assert uniform_prior.lower_limit == 2
+        assert uniform_prior.upper_limit == 3
+
+    def test_log_uniform(self, log_uniform_prior):
+        assert isinstance(log_uniform_prior, af.LogUniformPrior)
+        assert log_uniform_prior.lower_limit == 0.2
+        assert log_uniform_prior.upper_limit == 0.3
+
+    def test_gaussian(self, gaussian_prior):
+        assert isinstance(gaussian_prior, af.GaussianPrior)
+        assert gaussian_prior.lower_limit == -10
+        assert gaussian_prior.upper_limit == 10
+        assert gaussian_prior.mean == 3
+        assert gaussian_prior.sigma == 4
