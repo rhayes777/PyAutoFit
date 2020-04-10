43d7befaaa8cdb8c35338780c951fcb3df573c2f
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-04-10 09:34:20 +0100

    dataset uncommented

--- test_autofit/tools/test_dataset.py
+++ test_autofit/tools/test_dataset.py
@@ -2,6 +2,6 @@
 from test_autofit.mock import MockDataset
 
 
-# def test_load():
-#     dataset = MockDataset()
-#     assert dataset.name == MockDataset.load(f"/tmp/dataset.pickle").name
+def test_load():
+    dataset = MockDataset()
+    assert dataset.name == MockDataset.load(f"/tmp/dataset.pickle").name
