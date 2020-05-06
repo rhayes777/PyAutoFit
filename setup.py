43dbab0d10a42d21a01da5f9c999e0a0f443d91e
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-05-06 18:58:15 +0100

    README

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def version():
 
 
 this_dir = abspath(dirname(__file__))
-with open(join(this_dir, "README.md"), encoding="utf-8") as file:
+with open(join(this_dir, "README.rst"), encoding="utf-8") as file:
     long_description = file.read()
 
 setup(
