fcf4fe593acf7b743409843aeb39660d2672026d
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-05-09 12:06:24 +0100

    parallelization of Dynesty implemented.

--- test_autofit/unit/optimize/test_emcee.py
+++ test_autofit/unit/optimize/test_emcee.py
@@ -31,6 +31,7 @@ class TestEmceeConfig:
             auto_correlation_check_size=101,
             auto_correlation_required_length=51,
             auto_correlation_change_threshold=0.02,
+            number_of_cores=2,
         )
 
         assert emcee.nwalkers == 51
@@ -42,6 +43,7 @@ class TestEmceeConfig:
         assert emcee.auto_correlation_check_size == 101
         assert emcee.auto_correlation_required_length == 51
         assert emcee.auto_correlation_change_threshold == 0.02
+        assert emcee.number_of_cores == 2
 
         emcee = af.Emcee()
 
@@ -54,6 +56,7 @@ class TestEmceeConfig:
         assert emcee.auto_correlation_check_size == 100
         assert emcee.auto_correlation_required_length == 50
         assert emcee.auto_correlation_change_threshold == 0.01
+        assert emcee.number_of_cores == 1
 
     def test__samples_from_model(self):
 
@@ -149,4 +152,8 @@ class TestCopyWithNameExtension:
         assert (
             copy.auto_correlation_change_threshold
             is optimizer.auto_correlation_change_threshold
+        )
+        assert (
+            copy.number_of_cores
+            is optimizer.number_of_cores
         )
