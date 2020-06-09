ac0d3ccc645225d96a13237a880c374b68f57c01
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-06-09 14:19:50 +0100

    removed optimizer grid module

--- autofit/optimize/grid_search.py
+++ autofit/optimize/grid_search.py
@@ -10,7 +10,6 @@ from autoconf import conf
 from autofit import exc
 from autofit.mapper import model_mapper as mm
 from autofit.mapper.prior import prior as p
-from autofit.optimize import optimizer
 from autofit.optimize.non_linear.emcee import Emcee
 from autofit.optimize.non_linear.non_linear import Result
 from autofit.optimize.non_linear.paths import Paths
@@ -229,7 +228,7 @@ class GridSearch:
         -------
         lists: [[float]]
         """
-        return optimizer.make_lists(
+        return make_lists(
             len(grid_priors), step_size=self.hyper_step_size, centre_steps=False
         )
 
@@ -536,3 +535,63 @@ class Process(multiprocessing.Process):
                 self.queue.put(job.perform())
         logger.info("terminating process {}".format(self.name))
         self.job_queue.close()
+
+
+def grid(fitness_function, no_dimensions, step_size):
+    """
+    Grid search using a fitness function over a given number of dimensions and a given step size between inclusive
+    limits of 0 and 1.
+
+    Parameters
+    ----------
+    fitness_function: function
+        A function that takes a tuple of floats as an argument
+    no_dimensions: int
+        The number of dimensions of the grid search
+    step_size: float
+        The step size of the grid search
+
+    Returns
+    -------
+    best_arguments: tuple[float]
+        The tuple of arguments that gave the highest fitness
+    """
+    best_fitness = float("-inf")
+    best_arguments = None
+
+    for arguments in make_lists(no_dimensions, step_size):
+        fitness = fitness_function(tuple(arguments))
+        if fitness > best_fitness:
+            best_fitness = fitness
+            best_arguments = tuple(arguments)
+
+    return best_arguments
+
+
+def make_lists(no_dimensions, step_size, centre_steps=True):
+    """
+    Create a list of lists of floats covering every combination across no_dimensions of points of integer step size
+    between 0 and 1 inclusive.
+
+    Parameters
+    ----------
+    no_dimensions: int
+        The number of dimensions, that is the length of the lists
+    step_size: float
+        The step size
+    centre_steps: bool
+
+    Returns
+    -------
+    lists: [[float]]
+        A list of lists
+    """
+    if no_dimensions == 0:
+        return [[]]
+
+    sub_lists = make_lists(no_dimensions - 1, step_size, centre_steps=centre_steps)
+    return [
+        [step_size * value + (0.5 * step_size if centre_steps else 0)] + sub_list
+        for value in range(0, int((1 / step_size)))
+        for sub_list in sub_lists
+    ]
