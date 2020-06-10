e8cf1dec54fe875ce1a347437483981f5a70da55
Richard <richardhayes777@gmail.com>
2020-06-10 16:46:41 +0100

    example of looping

--- autofit/message_passing/__init__.py
+++ autofit/message_passing/__init__.py
@@ -2,3 +2,4 @@ from .factor_graphs import factor, Variable, Plate
 from .mean_field import FactorApproximation, MeanFieldApproximation
 from .messages import NormalMessage
 from .optimise import OptFactor
+from .sampling import ImportanceSampler, project_factor_approx_sample
