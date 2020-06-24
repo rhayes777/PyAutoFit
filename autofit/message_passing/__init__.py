1dfb7fe1f220ac0f476af0bd915e82168ff0ccad
Richard <richardhayes777@gmail.com>
2020-06-24 12:57:49 +0100

    running lotka volterra message passing

--- autofit/message_passing/__init__.py
+++ autofit/message_passing/__init__.py
@@ -1,5 +1,5 @@
 from .factor_graphs import factor, Variable, Plate
 from .mean_field import FactorApproximation, MeanFieldApproximation
-from .messages import NormalMessage, FracMessage, FixedMessage
-from .optimise import OptFactor
+from .messages import NormalMessage, FracMessage, FixedMessage, GammaMessage
+from .optimise import OptFactor, lstsq_laplace_factor_approx
 from .sampling import ImportanceSampler, project_factor_approx_sample
---
+++ test_autofit/unit/message_passing/test_lotka_volterra.py
@@ -0,0 +1,132 @@
+import numpy as np
+from scipy import integrate, stats
+
+from autofit import message_passing as mp
+
+
+def test():
+    ## define parameters of model
+    alpha, beta, gamma, delta = 2 / 3, 4 / 3, 1, 1
+    r = np.array([alpha, - gamma])
+    A = np.array([
+        [0., beta / alpha],
+        [delta / gamma, 0.]])
+    K = 1
+    noise = 0.1
+
+    # starting composition
+    y0 = np.array([1., 1.])
+
+    n_species = len(y0)
+    n_obs = 30
+    t_space = 1.
+    t_obs = np.r_[
+        0,
+        (np.arange(n_obs - 1) * t_space +
+         np.random.rand(n_obs - 1)) * t_space]
+
+    def lotka_volterra(t, z, r=r, A=A, K=K):
+        return z * r * (1 - A.dot(z) / K)
+
+    def calc_lotka_volterra(y0, r, A, K, t_obs):
+        res = integrate.solve_ivp(
+            lotka_volterra,
+            (t_obs[0], t_obs[-1]), y0, t_eval=t_obs,
+            args=(r, A, K),
+            method='BDF')
+
+        y_ = res.y
+        n = y_.shape[1]
+        n_obs = len(t_obs)
+        # make sure output is correct dimension
+        if n != n_obs:
+            y_ = np.c_[
+                     y_, np.repeat(y_[:, [-1]],
+                                   n_obs - n, axis=1)][:, :n_obs]
+            if y_.shape[1] != n_obs:
+                raise Exception
+
+        return y_
+
+    y_true = calc_lotka_volterra(y0, r, A, K, t_obs)
+    y = y_true + noise * np.random.randn(n_species, n_obs)
+
+    ## Specifying dimensions of problem
+    obs = mp.Plate(name='obs')
+    species = mp.Plate(name='species')
+    # Need to specify a second plate for species because
+    # A is (species, species) and we need a second plate
+    # to unique specify the second dimension
+    speciesA = mp.Plate(name='species')
+    dims = mp.Plate(name='dims')
+
+    ## Specifying variables
+    r_ = mp.Variable('r', species)
+    A_ = mp.Variable('A', species, speciesA)
+    K_ = mp.Variable('K')
+
+    y0_ = mp.Variable('y0', species)
+    y_ = mp.Variable('y', species, obs)
+
+    y_obs_ = mp.Variable('y_obs', species, obs)
+    t_obs_ = mp.Variable('t_obs', obs)
+
+    _norm = stats.norm(loc=0, scale=noise)
+    _prior = stats.norm(loc=0, scale=10)
+    _prior_exp = stats.expon(loc=0, scale=1)
+
+    def _likelihood(y_obs, y):
+        return _norm.logpdf(y_obs - y)
+
+    ## Specifying factors
+
+    likelihood = mp.factor(_likelihood)(y_obs_, y_)
+    prior_A = mp.factor(_prior.logpdf, 'prior_A')(A_)
+    prior_r = mp.factor(_prior.logpdf, 'prior_r')(r_)
+    prior_y0 = mp.factor(_prior_exp.logpdf, 'prior_y0')(y0_)
+
+    # calc_lotka_volterra does not vectorise over
+    # multiple inputs, see `FactorNode._py_vec_call`
+    LV = mp.factor(
+        calc_lotka_volterra, 'LV',
+        _vectorised=False)(y0_, r_, A_, K_, t_obs_) == y_
+
+    ## Defining model
+    priors = prior_A * prior_r * prior_y0
+    LV_model = (likelihood * LV) * priors
+    LV_model._name = 'LV_model'
+
+    model_approx = mp.MeanFieldApproximation.from_kws(
+        LV_model,
+        A=mp.NormalMessage.from_mode(A, 100.),
+        r=mp.NormalMessage.from_mode(r, 100.),
+        y0=mp.FracMessage(
+            mp.GammaMessage.from_mode(np.ones_like(y0), 1)),
+        y=mp.NormalMessage.from_mode(y, 1),
+        K=mp.FixedMessage(1),
+        y_obs=mp.FixedMessage(y),
+        t_obs=mp.FixedMessage(t_obs), )
+
+    history = {}
+    n_iter = 1
+    total = n_iter * len(LV_model.factors)
+
+    factors = [f for f in LV_model.factors if f not in (LV,)]
+
+    np.random.seed(1)
+
+    for i in range(n_iter):
+        # perform least squares fit for LV model
+        model_approx, status = mp.lstsq_laplace_factor_approx(
+            model_approx,
+            LV
+        )
+
+        # perform laplace non linear fit for other factors
+        for factor in factors:
+            model_approx, status = mp.optimise.laplace_factor_approx(
+                model_approx, factor, delta=1.)
+            history[i, factor] = model_approx
+
+    model_mean = {v: d.mean for v, d in model_approx.approx.items()}
+    y_pred = LV_model(**model_mean).deterministic_values['y']
