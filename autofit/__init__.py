1fd1c815ff88c6a97eb212fc30429e2bcc3e148f
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-07-03 13:10:09 +0100

    Incremented version number

--- autofit/__init__.py
+++ autofit/__init__.py
@@ -69,4 +69,4 @@ from .tools.phase_property import PhaseProperty
 from .tools.pipeline import Pipeline
 from .tools.pipeline import ResultsCollection
 
-__version__ = '0.62.0'
+__version__ = '0.62.1'
