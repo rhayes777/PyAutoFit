bfe6fc1b15612b8f6fe56b31097366e225d00239
Richard <richardhayes777@gmail.com>
2020-09-09 11:05:40 +0100

    removed old syntax

--- autofit/expectation_propagation/__init__.py
+++ autofit/expectation_propagation/__init__.py
@@ -1,6 +1,5 @@
 from .declarative import ModelFactor, LikelihoodModel
 from .factor_graphs import Plate, Variable, Factor, FactorGraph, AbstractFactor, FactorValue
-from .fascade import MeanFieldPriorModel, PriorVariable
 from .mean_field import FactorApproximation, MeanFieldApproximation
 from .messages import FixedMessage, NormalMessage, GammaMessage, AbstractMessage
 from .optimise import OptFactor, lstsq_laplace_factor_approx
