ba15947424f96fab8f72f7dba5fd2b37d73727f6
Matthew Griffiths <matthew@concr.co>
2020-09-11 15:09:21 +0100

    adding Variable and Plate to autofit.graphical import

--- autofit/graphical/__init__.py
+++ autofit/graphical/__init__.py
@@ -4,3 +4,4 @@ from .mean_field import FactorApproximation, MeanFieldApproximation
 from .messages import FixedMessage, NormalMessage, GammaMessage, AbstractMessage
 from .optimise import OptFactor, lstsq_laplace_factor_approx
 from .sampling import ImportanceSampler, project_factor_approx_sample
+from ..mapper.variable import Variable, Plate
