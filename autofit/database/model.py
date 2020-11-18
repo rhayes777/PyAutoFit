65d4af43d38c8084e85065779a8a4e314583a61f
Richard <richardhayes777@gmail.com>
2020-11-18 09:17:33 +0000

    introduce sqlalchemy

--- autofit/database/model.py
+++ autofit/database/model.py
@@ -1,2 +1,15 @@
-class PriorModel:
-    pass
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+_schema_version = 1
+
+
+class PriorModel(Base):
+    __tablename__ = "prior_model"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
