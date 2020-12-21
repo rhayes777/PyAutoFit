d0ad8c8f4fd2119862f9a6dacc5fee6411d5b0cd
Richard <richardhayes777@gmail.com>
2020-12-21 11:14:28 +0000

    querying works

--- autofit/database/__init__.py
+++ autofit/database/__init__.py
@@ -1,21 +1,80 @@
-from sqlalchemy import and_
-
 from .instance import *
 from .model import *
 from .prior import *
+
+
+class NameQuery:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def tables(self):
+        return ["object"]
+
+    @property
+    def conditions(self):
+        return [f"name = '{self.name}'"]
+
+    @property
+    def string(self):
+        tables_string = ", ".join(
+            self.tables
+        )
+        conditions_string = " AND ".join(
+            self.conditions
+        )
+        return f"SELECT parent_id FROM {tables_string} WHERE {conditions_string}"
+
+    def __eq__(self, other):
+        return EqualityQuery(
+            self.name,
+            other
+        )
+
+
+class EqualityQuery(NameQuery):
+    def __init__(self, name, value):
+        super().__init__(name)
+        self.value = value
+
+    @property
+    def tables(self):
+        return super().tables + ["value"]
+
+    @property
+    def conditions(self):
+        conditions = super().conditions + [
+            f"value = {self.value}"
+        ]
+
+        first_table = self.tables[0]
+        for table in self.tables[1:]:
+            conditions.append(
+                f"{table}.id = {first_table}.id"
+            )
+
+        return conditions
 
 
 class Aggregator:
     def __init__(self, session):
         self.session = session
 
-    def filter(self, **kwargs):
-        name, value = list(kwargs.items())[0]
-        return self.session.query(
-            Value
-        ).filter(
-            and_(
-                Value.name == name,
-                Value.value == value
+    def __getattr__(self, name):
+        return NameQuery(name)
+
+    def filter(self, predicate):
+        objects_ids = {
+            row[0]
+            for row
+            in self.session.execute(
+                predicate.string
             )
-        ).one().parent
+        }
+        return self.session.query(
+            Object
+        ).filter(
+            Object.id.in_(
+                objects_ids
+            )
+        ).all()
--- test_autofit/unit/database/test_query.py
+++ test_autofit/unit/database/test_query.py
@@ -1,11 +1,34 @@
-from sqlalchemy import and_, exists
+import pytest
 
 from autofit import database as db
 from autofit.mock import mock as m
 
 
-def test_query(
+@pytest.fixture(
+    name="aggregator"
+)
+def make_aggregator(
         session
+):
+    return db.Aggregator(session)
+
+
+def test_attribute_query(
+        aggregator
+):
+    assert aggregator.centre.string == "SELECT parent_id FROM object WHERE name = 'centre'"
+
+
+def test_equality_query(
+        aggregator
+):
+    string = "SELECT parent_id FROM object, value WHERE name = 'centre' AND value = 1 AND value.id = object.id"
+    assert (aggregator.centre == 1).string == string
+
+
+def test_query(
+        session,
+        aggregator
 ):
     gaussian_1 = db.Object.from_object(
         m.Gaussian(
@@ -24,8 +47,8 @@ def test_query(
     ])
     session.commit()
 
-    result = db.Aggregator(session).filter(
-        centre=1
+    result = aggregator.filter(
+        aggregator.centre == 1
     )
 
     assert result == [gaussian_1]
