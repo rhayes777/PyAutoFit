f04444803d8ce994c2ba6b50ab343ece670c1875
Richard <richardhayes777@gmail.com>
2021-01-20 11:02:40 +0000

    group tests

--- test_autofit/unit/database/query/test_new_model.py
+++ test_autofit/unit/database/query/test_new_model.py
@@ -1,31 +1,31 @@
 from autofit.database import query_model as q
 
 
-def test_named_query():
-    query = q.NamedQuery(
-        "a"
-    )
-
-    assert query == (
-        "SELECT parent_id "
-        "FROM object AS o "
-        "WHERE o.name = 'a'"
-    )
-
-
-def test_with_value():
-    query = q.NamedQuery(
-        "a",
-        q.ValueCondition(
-            "=",
-            1
+class TestString:
+    def test_named_query(self):
+        query = q.NamedQuery(
+            "a"
         )
-    )
 
-    assert query == (
-        "SELECT parent_id "
-        "FROM object AS o, "
-        "value AS v "
-        "WHERE o.name = 'a' "
-        "AND v.value = 1"
-    )
+        assert query == (
+            "SELECT parent_id "
+            "FROM object AS o "
+            "WHERE o.name = 'a'"
+        )
+
+    def test_with_value(self):
+        query = q.NamedQuery(
+            "a",
+            q.ValueCondition(
+                "=",
+                1
+            )
+        )
+
+        assert query == (
+            "SELECT parent_id "
+            "FROM object AS o, "
+            "value AS v "
+            "WHERE o.name = 'a' "
+            "AND v.value = 1"
+        )
