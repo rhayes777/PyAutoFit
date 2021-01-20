449e82c4513aec3067504bfa30c59a9d92203859
Richard <richardhayes777@gmail.com>
2021-01-20 11:00:41 +0000

    basic query construction

--- autofit/database/query_model/__init__.py
+++ autofit/database/query_model/__init__.py
@@ -1,10 +1,107 @@
-class NamedQuery:
+from abc import ABC, abstractmethod
+
+
+class Table:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def abbreviation(self):
+        return self.name[0]
+
     def __str__(self):
-        return f"SELECT parent_id FROM object as o WHERE name = '{self.name}'"
+        return f"{self.name} AS {self.abbreviation}"
+
+
+object_table = Table("object")
+value_table = Table("value")
+
+
+class AbstractCondition(ABC):
+    @property
+    @abstractmethod
+    def tables(self):
+        pass
+
+    @property
+    def tables_string(self):
+        return ", ".join(sorted(map(str, self.tables)))
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    def __and__(self, other):
+        return And(
+            self,
+            other
+        )
+
+
+class ValueCondition(AbstractCondition):
+    def __init__(self, symbol, value):
+        self.value = value
+        self.symbol = symbol
+
+    @property
+    def tables(self):
+        return {value_table}
+
+    def __str__(self):
+        return f"{value_table.abbreviation}.value {self.symbol} {self.value}"
+
+
+class NameCondition(AbstractCondition):
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def tables(self):
+        return {object_table}
+
+    def __str__(self):
+        return f"{object_table.abbreviation}.name = '{self.name}'"
+
+
+class And(AbstractCondition):
+    def __init__(
+            self,
+            condition_1: AbstractCondition,
+            condition_2: AbstractCondition
+    ):
+        self.condition_1 = condition_1
+        self.condition_2 = condition_2
+
+    @property
+    def tables(self):
+        return {
+            *self.condition_1.tables,
+            *self.condition_2.tables
+        }
+
+    def __str__(self):
+        return f"{self.condition_1} AND {self.condition_2}"
+
+
+class NamedQuery:
+    def __init__(
+            self,
+            name,
+            condition=None
+    ):
+        self.name = name
+        self.condition = NameCondition(
+            self.name
+        )
+
+        if condition is not None:
+            self.condition &= condition
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f"SELECT parent_id FROM {self.condition.tables_string} WHERE {self.condition}"
 
     def __eq__(self, other):
         return str(self) == str(other)
-
--- test_autofit/unit/database/query/test_new_model.py
+++ test_autofit/unit/database/query/test_new_model.py
@@ -8,6 +8,24 @@ def test_named_query():
 
     assert query == (
         "SELECT parent_id "
-        "FROM object as o "
-        "WHERE name = 'a'"
+        "FROM object AS o "
+        "WHERE o.name = 'a'"
+    )
+
+
+def test_with_value():
+    query = q.NamedQuery(
+        "a",
+        q.ValueCondition(
+            "=",
+            1
+        )
+    )
+
+    assert query == (
+        "SELECT parent_id "
+        "FROM object AS o, "
+        "value AS v "
+        "WHERE o.name = 'a' "
+        "AND v.value = 1"
     )
