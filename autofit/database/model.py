c7e5553507761a8d2867a582ced93a798d422a70
Richard <richardhayes777@gmail.com>
2021-01-27 16:16:27 +0000

    more docsa

--- autofit/database/model.py
+++ autofit/database/model.py
@@ -203,5 +203,8 @@ class Object(Base):
         self.class_path = get_class_path(cls)
 
 
-def get_class_path(cls):
+def get_class_path(cls: type) -> str:
+    """
+    The full import path of the type
+    """
     return re.search("'(.*)'", str(cls))[1]
--- autofit/database/query_model/condition.py
+++ autofit/database/query_model/condition.py
@@ -128,52 +128,98 @@ class AbstractValueCondition(AbstractCondition, ABC):
 class ValueCondition(AbstractValueCondition):
     @property
     def tables(self) -> Set[Table]:
+        """
+        Tables included in the condition string
+        """
         return {value_table}
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        The condition in SQL
+        """
         return f"{value_table.abbreviation}.value {self.symbol} {self.value}"
 
 
 class StringValueCondition(AbstractValueCondition):
     @property
     def tables(self):
+        """
+        Tables included in the condition string
+        """
         return {string_value_table}
 
     def __str__(self):
+        """
+        The condition in SQL
+        """
         return f"{string_value_table.abbreviation}.value {self.symbol} '{self.value}'"
 
 
 class NameCondition(AbstractCondition):
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """
+        Condition used to match the name of an object. e.g. galaxy, lens, brightness.
+
+        Parameters
+        ----------
+        name
+            The name of an attribute in the model
+        """
         self.name = name
 
     @property
     def tables(self):
+        """
+        Tables included in the condition string
+        """
         return {object_table}
 
     def __str__(self):
+        """
+        The condition in SQL
+        """
         return f"{object_table.abbreviation}.name = '{self.name}'"
 
 
 class TypeCondition(AbstractCondition):
-    def __init__(self, cls):
+    def __init__(self, cls: type):
+        """
+        Condition used to match the type of an object. e.g. SersicLightProfile
+
+        Parameters
+        ----------
+        cls
+            The type
+        """
         self.cls = cls
 
     @property
     def tables(self):
+        """
+        Tables included in the condition string
+        """
         return {object_table}
 
+    def __str__(self):
+        """
+        The condition in SQL
+        """
+        return f"{object_table.abbreviation}.class_path = '{self.class_path}'"
+
     @property
-    def class_path(self):
+    def class_path(self) -> str:
+        """
+        The full import path of the type
+        """
         return get_class_path(
             self.cls
         )
 
-    def __str__(self):
-        return f"{object_table.abbreviation}.class_path = '{self.class_path}'"
 
-
-def exclude_null(func):
+def exclude_none(func):
+    """
+    Decorator that filters None from an argument list of conditions
+    """
     @wraps(func)
     def wrapper(arg, *conditions):
         conditions = list(filter(
@@ -186,17 +232,44 @@ def exclude_null(func):
 
 
 class AbstractJunction(AbstractCondition, ABC):
-    @exclude_null
-    def __new__(cls, *conditions):
+    @exclude_none
+    def __new__(
+            cls,
+            *conditions: AbstractCondition
+    ):
+        """
+        If only a single extant condition is passed in then that
+        condition should simply be returned.
+        """
         if len(conditions) == 1:
             return conditions[0]
         return object.__new__(cls)
 
-    @exclude_null
+    @exclude_none
     def __init__(
             self,
             *conditions: AbstractCondition
     ):
+        """
+        A combination of two or more conditions. A set of rules allow
+        the query to be expressed in the simplest terms possible
+
+        Any subjunctions of the same type are unwrapped.
+        i.e. And(A, B) & And(C, D) -> And(A, B, C, D)
+
+        Conditions are a set so duplication is removed.
+        i.e. And(A, B, A) -> And(A, B)
+
+        NamedQuery child conditions are matched by their name creating
+        one NameQuery for each name and applying the junction type to
+        their conditions.
+        i.e. And(Named('name', A), Named('name', B)) -> Named('name', And(A, B))
+
+        Parameters
+        ----------
+        conditions
+            A list of SQL conditions
+        """
         from .query import NamedQuery
 
         self.conditions = set()
@@ -243,7 +316,11 @@ class AbstractJunction(AbstractCondition, ABC):
         return iter(sorted(self.conditions))
 
     @property
-    def tables(self):
+    def tables(self) -> Set[Table]:
+        """
+        Combines the tables of all subqueries which are not
+        named queries
+        """
         from .query import NamedQuery
         return {
             table
@@ -259,10 +336,15 @@ class AbstractJunction(AbstractCondition, ABC):
 
     @property
     @abstractmethod
-    def join(self):
-        pass
+    def join(self) -> str:
+        """
+        SQL string used to conjoin queries
+        """
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        SQL string expressing combined query
+        """
         return f" {self.join} ".join(map(
             str,
             sorted(
@@ -273,7 +355,7 @@ class AbstractJunction(AbstractCondition, ABC):
 
 class And(AbstractJunction):
     @property
-    def join(self):
+    def join(self) -> str:
         return "AND"
 
 
