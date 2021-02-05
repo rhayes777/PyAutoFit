db3d13994c406a62b26528e9600bff6ac4c08596
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-02-05 17:39:07 +0000

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -106,6 +106,9 @@ html_context = {
     "github_version": "development",
 }
 
+def setup(app):
+    app.add_css_file('custom.css')
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
