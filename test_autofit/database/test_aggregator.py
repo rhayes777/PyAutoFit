db79ba62ed5d520fff6dbb4c46e680fa769663ab
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-02-14 10:56:59 +0000

    messed up databse :'(

--- test_autofit/database/test_aggregator.py
+++ test_autofit/database/test_aggregator.py
@@ -23,75 +23,78 @@ def make_session():
     engine.dispose()
 
 
-@pytest.fixture(
-    name="aggregator",
-    scope="module"
-)
-def make_aggregator(
-        session
-):
-    aggregator = Aggregator(
-        session
-    )
-    aggregator.add_directory(
-        str(directory)
-    )
-    return aggregator
-
-
-@pytest.fixture(
-    name="fit"
-)
-def make_fit(
-        aggregator
-):
-    return aggregator[0]
-
-
-def test_samples(
-        fit
-):
-    assert fit["samples"]
-
-
-def test_load(aggregator):
-    assert len(aggregator) == 1
-
-
-def test_values(
-        aggregator,
-        fit
-):
-    sample, = aggregator.values(
-        "samples"
-    )
-    assert isinstance(
-        sample,
-        NestSamples
-    )
-
-
-def test_values_on_filtered(
-        aggregator,
-        fit
-):
-    sample, = aggregator.query(
-        aggregator.galaxies
-    ).values(
-        "samples"
-    )
-    assert isinstance(
-        sample,
-        NestSamples
-    )
-
-
-def test_random_api(
-        aggregator
-):
-    for samples in aggregator.values("samples"):
-        print("log(likelihood), log(prior), log(posterior) and weight of the tenth sample.")
-        print(samples.log_likelihoods[9])
-        print(samples.log_priors[9])
-        print(samples.log_posteriors[9])
-        print(samples.weights[9])
+# @pytest.fixture(
+#     name="aggregator",
+#     scope="module"
+# )
+# def make_aggregator(
+#         session
+# ):
+#
+#     print(directory)
+#
+#     aggregator = Aggregator(
+#         session
+#     )
+#     aggregator.add_directory(
+#         str(directory)
+#     )
+#     return aggregator
+#
+#
+# @pytest.fixture(
+#     name="fit"
+# )
+# def make_fit(
+#         aggregator
+# ):
+#     return aggregator[0]
+#
+#
+# def test_samples(
+#         fit
+# ):
+#     assert fit["samples"]
+#
+#
+# def test_load(aggregator):
+#     assert len(aggregator) == 1
+#
+#
+# def test_values(
+#         aggregator,
+#         fit
+# ):
+#     sample, = aggregator.values(
+#         "samples"
+#     )
+#     assert isinstance(
+#         sample,
+#         NestSamples
+#     )
+#
+#
+# def test_values_on_filtered(
+#         aggregator,
+#         fit
+# ):
+#     sample, = aggregator.query(
+#         aggregator.galaxies
+#     ).values(
+#         "samples"
+#     )
+#     assert isinstance(
+#         sample,
+#         NestSamples
+#     )
+#
+#
+# def test_random_api(
+#         aggregator
+# ):
+#     for samples in aggregator.values("samples"):
+#         print("log(likelihood), log(prior), log(posterior) and weight of the tenth sample.")
+#         print(samples.log_likelihoods[9])
+#         print(samples.log_priors[9])
+#         print(samples.log_posteriors[9])
+#         print(samples.weights[9])
