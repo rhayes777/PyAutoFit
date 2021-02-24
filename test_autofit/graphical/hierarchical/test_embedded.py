82f67acb6c20781086d0858ccb19f70beb1568ff
Richard <richardhayes777@gmail.com>
2021-02-24 12:57:04 +0000

    setup data generationa

--- test_autofit/graphical/hierarchical/test_embedded.py
+++ test_autofit/graphical/hierarchical/test_embedded.py
@@ -1,20 +1,71 @@
+import numpy as np
+import pytest
+
 import autofit as af
 
+x = np.arange(100)
+n = 10
 
-def test_embedded_priors():
-    prior_model = af.PriorModel(
+
+@pytest.fixture(
+    name="centre_model"
+)
+def make_centre_model():
+    return af.PriorModel(
         af.GaussianPrior,
         mean=af.GaussianPrior(
-            mean=0,
-            sigma=1
+            mean=50,
+            sigma=10
         ),
         sigma=af.GaussianPrior(
-            mean=4,
-            sigma=1
+            mean=10,
+            sigma=5
         )
     )
 
+
+def test_embedded_priors(
+        centre_model
+):
     assert isinstance(
-        prior_model.random_instance().value_for(0.5),
+        centre_model.random_instance().value_for(0.5),
         float
     )
+
+
+@pytest.fixture(
+    name="data"
+)
+def generate_data(
+        centre_model
+):
+    data = []
+    for _ in range(n):
+        instance = centre_model.random_instance()
+        data.append(
+            np.array(list(map(
+                instance,
+                x
+            )))
+        )
+    return data
+
+
+def test_generate_data(
+        data
+):
+    print(data)
+
+# class Analysis:
+#     def __init__(self, x, y, sigma=.04):
+#         self.x = x
+#         self.y = y
+#         self.sigma = sigma
+#
+#     def log_likelihood_function(self, instance: Gaussian) -> np.array:
+#         """
+#         This function takes an instance created by the PriorModel and computes the
+#         likelihood that it fits the data.
+#         """
+#         y_model = instance(self.x)
+#         return np.sum(_likelihood(y_model, self.y) / self.sigma**2)
