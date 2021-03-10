868edc4b06724cb043df2aa52524b62dad20bd0f
Richard <richardhayes777@gmail.com>
2021-03-10 10:03:54 +0000

    more refactoring

--- autofit/database/aggregator/aggregator.py
+++ autofit/database/aggregator/aggregator.py
@@ -1,12 +1,11 @@
-import os
 from typing import Optional, List
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
-from autofit.aggregator.aggregator import Aggregator as ClassicAggregator
 from autofit.database import query as q
 from .abstract import AbstractAggregator
+from .scrape import scrape_directory
 from .. import model as m
 from ..query.query import AbstractQuery
 
@@ -148,34 +147,9 @@ class Aggregator(AbstractAggregator):
             A directory containing autofit results embedded in a
             file structure
         """
-        aggregator = ClassicAggregator(
-            directory
-        )
-        for item in aggregator:
-            model = item.model
-            samples = item.samples
-            instance = samples.max_log_likelihood_instance
-            fit = m.Fit(
-                model=model,
-                instance=instance,
-                phase_name=item.name
-            )
-
-            pickle_path = item.pickle_path
-            for pickle_name in os.listdir(
-                    pickle_path
-            ):
-                with open(
-                        os.path.join(
-                            pickle_path,
-                            pickle_name
-                        ),
-                        "r+b"
-                ) as f:
-                    fit[pickle_name.replace(
-                        ".pickle",
-                        ""
-                    )] = f.read()
+        for fit in scrape_directory(
+                directory
+        ):
 
             #    fit.dataset_name = fit["dataset"].name
             self.session.add(
