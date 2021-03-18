06804d8de28131ab4ae0abb99888c5326b7649a1
Richard <richardhayes777@gmail.com>
2021-03-18 16:29:58 +0000

    independent identifier class

--- autofit/mapper/model_object.py
+++ autofit/mapper/model_object.py
@@ -3,6 +3,54 @@ from collections import Iterable
 from hashlib import md5
 
 
+class Identifier:
+    def __init__(self, obj):
+        self.hash_list = list()
+        self._add_value_to_hash_list(
+            obj
+        )
+
+    def _add_value_to_hash_list(self, value):
+        if hasattr(value, "__dict__"):
+            for key, value in value.__dict__.items():
+                if not (key.startswith("_") or key == "id"):
+                    self.hash_list.append(key)
+                    self.add_value_to_hash_list(
+                        value
+                    )
+        elif isinstance(value, Iterable):
+            for value in value:
+                self.add_value_to_hash_list(
+                    value
+                )
+        elif isinstance(
+                value,
+                (str, float, int)
+        ):
+            self.hash_list.append(
+                str(value)
+            )
+
+    def add_value_to_hash_list(self, value):
+        if hasattr(value, "identifier"):
+            self.hash_list.append(
+                value.identifier
+            )
+        else:
+            self._add_value_to_hash_list(
+                value
+            )
+        self._add_value_to_hash_list(value)
+        return md5(".".join(
+            self.hash_list
+        ).encode("utf-8")).hexdigest()
+
+    def __str__(self):
+        return md5(".".join(
+            self.hash_list
+        ).encode("utf-8")).hexdigest()
+
+
 class ModelObject:
     _ids = itertools.count()
 
@@ -24,40 +72,4 @@ class ModelObject:
 
     @property
     def identifier(self):
-        hash_list = list()
-
-        def _add_value_to_hash_list(value):
-            if hasattr(value, "__dict__"):
-                for key, value in value.__dict__.items():
-                    if not (key.startswith("_") or key == "id"):
-                        hash_list.append(key)
-                        add_value_to_hash_list(
-                            value
-                        )
-            elif isinstance(value, Iterable):
-                for value in value:
-                    add_value_to_hash_list(
-                        value
-                    )
-            elif isinstance(
-                    value,
-                    (str, float, int)
-            ):
-                hash_list.append(
-                    str(value)
-                )
-
-        def add_value_to_hash_list(value):
-            if hasattr(value, "identifier"):
-                hash_list.append(
-                    value.identifier
-                )
-            else:
-                _add_value_to_hash_list(
-                    value
-                )
-
-        _add_value_to_hash_list(self)
-        return md5(".".join(
-            hash_list
-        ).encode("utf-8")).hexdigest()
+        return str(Identifier(self))
