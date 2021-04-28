5ea03c6b8ac675411d2cd541c603d30ad749835b
Richard <richardhayes777@gmail.com>
2021-04-28 09:15:37 +0100

    identifier fields

--- test_autofit/database/identifier/test_identifiers.py
+++ test_autofit/database/identifier/test_identifiers.py
@@ -1,6 +1,32 @@
 import autofit as af
 from autofit import database as db
+from autofit.mapper.model_object import Identifier
 from autofit.mock.mock import Gaussian
+
+
+class Class:
+    def __init__(self, one=1, two=2, three=3):
+        self.one = one
+        self.two = two
+        self.three = three
+
+    __identifier_fields__ = ("one", "two")
+
+
+def test_identifier_fields():
+    other = Class(three=4)
+    assert Identifier(
+        Class()
+    ) == Identifier(
+        other
+    )
+
+    other.__identifier_fields__ = ("one", "two", "three")
+    assert Identifier(
+        Class()
+    ) != Identifier(
+        other
+    )
 
 
 def test_fit():
