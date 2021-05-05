6fca3d40ad3aec6a9e7a17f4ce02522596f0800e
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-05-05 15:53:02 +0100

    function -> func

--- autofit/database/aggregator/aggregator.py
+++ autofit/database/aggregator/aggregator.py
@@ -112,9 +112,9 @@ class Aggregator:
             )
         return self._fits
 
-    def map(self, function):
+    def map(self, func):
         for fit in self.fits:
-            yield function(fit)
+            yield func(fit)
 
     def __repr__(self):
         return f"<{self.__class__.__name__} {self.filename} {len(self)}>"
