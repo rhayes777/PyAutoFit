1ce66e1be964091092d5fdae1e77d7b56dced30d
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-05-14 09:22:01 +0100

    prints processes

--- autofit/non_linear/abstract_search.py
+++ autofit/non_linear/abstract_search.py
@@ -22,10 +22,6 @@ from autofit.non_linear.paths.directory import DirectoryPaths
 from autofit.non_linear.result import Result
 from autofit.non_linear.timer import Timer
 
-import sys
-
-log_file = open("message.log", "w")
-
 class NonLinearSearch(ABC):
     def __init__(
             self,
@@ -201,16 +197,7 @@ class NonLinearSearch(ABC):
                 if log_likelihood > self.log_likelihood_cap:
                     log_likelihood = self.log_likelihood_cap
 
-            old_stdout = sys.stdout
-            log_file = open("message.log", "a")
-
-            sys.stdout = log_file
-
             print(mp.current_process().pid, log_likelihood)
-
-            sys.stdout = old_stdout
-
-            log_file.close()
 
             return log_likelihood
 
