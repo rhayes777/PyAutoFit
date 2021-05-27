79ab9e651ec2ab1703e573ff7d54075b2431bcfa
Richard <richardhayes777@gmail.com>
2021-05-27 15:48:34 +0100

    copy and remove database file

--- test_autofit/database/migration/test_steps_match.py
+++ test_autofit/database/migration/test_steps_match.py
@@ -1,11 +1,36 @@
+import os
+import shutil
 from pathlib import Path
 
+import pytest
+
 import autofit as af
+
+origin = Path(
+    __file__
+).parent / "database.sqlite"
+
+copy = Path(
+    __file__
+).parent / "database_copy.sqlite"
+
+
+@pytest.fixture(
+    autouse=True
+)
+def manage_files():
+    shutil.copy(
+        origin,
+        copy
+    )
+    yield
+    os.remove(
+        copy,
+
+    )
 
 
 def test():
     af.Aggregator.from_database(
-        Path(
-            __file__
-        ).parent / "database.sqlite"
+        copy
     )
