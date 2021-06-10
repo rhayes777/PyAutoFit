f05244da6d868673a355df11858823eeff0c35f5
Richard <richardhayes777@gmail.com>
2021-06-10 16:14:32 +0100

    fixed tests and dropped case

--- test_autofit/tools/test_edenise/test_structure.py
+++ test_autofit/tools/test_edenise/test_structure.py
@@ -13,7 +13,7 @@ top_level_path = "top_level/path"
 def make_package():
     directory = Path(
         __file__
-    ).parent.parent.parent / "autofit"
+    ).parent.parent.parent.parent / "autofit"
     return Package.from_directory(
         directory,
         prefix="VIS_CTI"
@@ -27,39 +27,42 @@ def make_child(package):
     return package.children[0]
 
 
-class TestPackageStructure:
-    def test_top_level(self, package):
-        assert package.is_top_level is True
-        assert len(package.children) > 1
+def test_top_level(package):
+    assert package.is_top_level is True
+    assert len(package.children) > 1
 
-    def test_child(self, child):
-        assert isinstance(
-            child,
-            Package
-        )
-        assert child.is_top_level is False
 
-    def test_path(self, package, child):
-        assert package.target_name == "VIS_CTI_Autofit"
-        assert str(package.target_path) == "VIS_CTI_Autofit"
-        assert str(child.target_path) == f"VIS_CTI_Autofit/{child.target_name}"
+def test_child(child):
+    assert isinstance(
+        child,
+        Package
+    )
+    assert child.is_top_level is False
 
-    def test_import(self, package):
-        import_ = Import("from autofit.tools.edenise import Line")
-        import_.parent = package
-        assert "autofit/tools/edenise/converter.py" in str(import_.path)
-        assert import_.is_in_project is True
 
-        import_ = Import("import os")
-        import_.parent = package
-        assert import_.is_in_project is False
+def test_path(package, child):
+    assert package.target_name == "VIS_CTI_Autofit"
+    assert str(package.target_path) == "VIS_CTI_Autofit"
+    assert str(child.target_path) == f"VIS_CTI_Autofit/{child.target_name}"
 
-    def test_file(self, package):
-        file = File(
-            Path(__file__)
-        )
 
-        file.parent = package
+def test_import(package):
+    import_ = Import("from autofit.tools.edenise import Line")
+    import_.parent = package
+    assert "autofit/tools/edenise/converter.py" in str(import_.path)
+    assert import_.is_in_project is True
 
-        assert len(file.imports) > 1
-        assert len(file.project_imports) == 1
+    import_ = Import("import os")
+    import_.parent = package
+    assert import_.is_in_project is False
+
+
+def test_file(package):
+    file = File(
+        Path(__file__)
+    )
+
+    file.parent = package
+
+    assert len(file.imports) > 1
+    assert len(file.project_imports) == 1
