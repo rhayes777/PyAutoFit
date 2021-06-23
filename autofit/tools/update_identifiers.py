0f7d78f8306dbb3b98e8e6a585b7132976e31a00
Richard <richardhayes777@gmail.com>
2021-06-23 11:49:18 +0100

    update identifiers

--- autofit/tools/update_identifiers.py
+++ autofit/tools/update_identifiers.py
@@ -5,8 +5,23 @@ from autofit.aggregator import Aggregator
 
 
 def update_identifiers(
-        output_directory
+        output_directory: str
 ):
+    """
+    Update identifiers in a given directory.
+
+    When identifiers were computed through an out of date method this
+    can be used to move the data to a new directory with the correct
+    identifier.
+
+    search.pickle is replaced to ensure its internal identifier matches
+    that of the directory.
+
+    Parameters
+    ----------
+    output_directory
+        A directory containing output results
+    """
     aggregator = Aggregator(
         output_directory
     )
