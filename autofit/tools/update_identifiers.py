598430b614a947c4854cb46b1dc594ac15cd1a9b
Richard <richardhayes777@gmail.com>
2021-06-23 11:12:50 +0100

    demonstrating changing identifier

---
+++ autofit/tools/update_identifiers.py
@@ -0,0 +1,2 @@
+def update_identifiers():
+    pass
--- test_autofit/database/identifier/test_converter.py
+++ test_autofit/database/identifier/test_converter.py
@@ -1,8 +1,20 @@
+from pathlib import Path
+
 import autofit as af
+from autoconf.conf import output_path_for_test
 from autofit import Gaussian
+from autofit import conf
+
+output_directory = Path(
+    __file__
+).parent / "output"
 
 
+@output_path_for_test(
+    output_directory
+)
 def test_consistent_identifier():
+    conf.instance["general"]["output"]["identifier_version"] = 1
     search = af.DynestyStatic(
         name="name"
     )
@@ -14,9 +26,12 @@ def test_consistent_identifier():
         info={},
         pickle_files=[]
     )
+
+    conf.instance["general"]["output"]["identifier_version"] = 3
     search_output = af.SearchOutput(
         search.paths.output_path
     )
+    search_output.search.paths._identifier = None
     assert search_output.model is not None
-    assert search_output.search.paths.identifier == search.paths.identifier
+    assert search_output.search.paths.identifier != search.paths.identifier
     print(search.paths.output_path)
