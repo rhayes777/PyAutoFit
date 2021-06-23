63f39b50401ab9c7da2888c3ad1badfba2e902e4
Richard <richardhayes777@gmail.com>
2021-06-23 16:59:13 +0100

    parameterization

---
+++ test_autofit/mapper/test_parameterization.py
@@ -0,0 +1,14 @@
+import autofit as af
+
+
+def test_parameterization():
+    model = af.Collection(
+        collection=af.Collection(
+            gaussian=af.Model(af.Gaussian)
+        )
+    )
+
+    parameterization = model.parameterization
+    assert parameterization == (
+        'collection->gaussian:                                                       Gaussian (N=3)'
+    )
