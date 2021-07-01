73c4296a189476302de53ca5c37a8c76e4ec971c
Richard <richardhayes777@gmail.com>
2021-07-01 16:09:20 +0100

    updated tests so they don't depend on equality

--- test_autofit/database/query/internal/test_construction.py
+++ test_autofit/database/query/internal/test_construction.py
@@ -3,12 +3,12 @@ from autofit.mock import mock
 
 
 def test_simple(aggregator):
-    assert aggregator.centre == q.Q("centre")
+    assert aggregator.centre.query == q.Q("centre").query
 
 
 def test_and(aggregator):
     construction = ((aggregator.centre == mock.Gaussian) & (aggregator.centre.x == 0))
-    assert construction == q.Q(
+    assert construction.query == q.Q(
         "centre",
         q.And(
             q.T(mock.Gaussian),
@@ -19,12 +19,12 @@ def test_and(aggregator):
                 )
             )
         )
-    )
+    ).query
 
 
 def test_or(aggregator):
     construction = ((aggregator.centre == mock.Gaussian) | (aggregator.centre.x == 0))
-    assert construction == q.Q(
+    assert construction.query == q.Q(
         "centre",
         q.Or(
             q.T(mock.Gaussian),
@@ -35,40 +35,40 @@ def test_or(aggregator):
                 )
             )
         )
-    )
+    ).query
 
 
 def test_with_value(aggregator):
-    assert (aggregator.centre == 1) == q.Q("centre", q.V("=", 1))
+    assert (aggregator.centre == 1).query == q.Q("centre", q.V("=", 1)).query
 
 
 def test_second_level(aggregator):
-    assert (aggregator.lens.centre == 1) == q.Q("lens", q.Q("centre", q.V("=", 1)))
+    assert (aggregator.lens.centre == 1).query == q.Q("lens", q.Q("centre", q.V("=", 1))).query
 
 
 def test_third_level(aggregator):
-    assert (aggregator.lens.centre.x == 1) == q.Q("lens", q.Q("centre", q.Q("x", q.V("=", 1))))
+    assert (aggregator.lens.centre.x == 1).query == q.Q("lens", q.Q("centre", q.Q("x", q.V("=", 1)))).query
 
 
 def test_with_type(aggregator):
-    assert (aggregator.centre == mock.Gaussian) == q.Q("centre", q.T(mock.Gaussian))
+    assert (aggregator.centre == mock.Gaussian).query == q.Q("centre", q.T(mock.Gaussian)).query
 
 
 def test_with_string(aggregator):
-    assert (aggregator.centre == "centre") == q.Q("centre", q.SV("=", "centre"))
+    assert (aggregator.centre == "centre").query == q.Q("centre", q.SV("=", "centre")).query
 
 
 def test_greater_than(aggregator):
-    assert (aggregator.centre > 1) == q.Q("centre", q.V(">", 1))
+    assert (aggregator.centre > 1).query == q.Q("centre", q.V(">", 1)).query
 
 
 def test_less_than(aggregator):
-    assert (aggregator.centre < 1) == q.Q("centre", q.V("<", 1))
+    assert (aggregator.centre < 1).query == q.Q("centre", q.V("<", 1)).query
 
 
 def test_less_than_equals(aggregator):
-    assert (aggregator.centre <= 1) == q.Q("centre", q.V("<=", 1))
+    assert (aggregator.centre <= 1).query == q.Q("centre", q.V("<=", 1)).query
 
 
 def test_greater_than_equals(aggregator):
-    assert (aggregator.centre >= 1) == q.Q("centre", q.V(">=", 1))
+    assert (aggregator.centre >= 1).query == q.Q("centre", q.V(">=", 1)).query
