401ebf725816aba5e16f14b5dd19afc58187d776
Richard <richardhayes777@gmail.com>
2021-08-04 15:57:00 +0100

    extracted fixtures

--- test_autofit/non_linear/grid/test_result.py
+++ test_autofit/non_linear/grid/test_result.py
@@ -3,6 +3,28 @@ import pytest
 import autofit as af
 
 
+@pytest.fixture(
+    name="model"
+)
+def make_model():
+    return af.Model(
+        af.Gaussian
+    )
+
+
+@pytest.fixture(
+    name="result"
+)
+def make_result(model):
+    return af.GridSearchResult(
+        results=[],
+        lower_limit_lists=[
+            [0.5]
+        ],
+        grid_priors=[model.centre]
+    )
+
+
 @pytest.mark.parametrize(
     "upper_limit, physical_value",
     [
@@ -13,21 +35,11 @@ import autofit as af
 )
 def test_physical_lower_limits(
         upper_limit,
-        physical_value
+        physical_value,
+        model,
+        result
 ):
-    model = af.Model(
-        af.Gaussian,
-        centre=af.UniformPrior(
-            lower_limit=0.0,
-            upper_limit=upper_limit
-        )
-    )
-    result = af.GridSearchResult(
-        results=[],
-        lower_limit_lists=[
-            [0.5]
-        ],
-        grid_priors=[model.centre]
-    )
-
-    assert result.physical_lower_limits_lists == [[physical_value]]
+    model.centre.upper_limit = upper_limit
+    assert result.physical_lower_limits_lists == [[
+        physical_value
+    ]]
