7830a5b4a1c37288aa6c6beefe6800127cf76881
Richard <richardhayes777@gmail.com>
2021-08-11 15:45:59 +0100

    unique tag

--- test_autofit/database/query/test_grid_seach.py
+++ test_autofit/database/query/test_grid_seach.py
@@ -37,10 +37,25 @@ def make_children():
 def make_grid_fit(children):
     return db.Fit(
         id="grid",
+        unique_tag="grid_fit_1",
         is_grid_search=True,
         children=children,
         instance=Gaussian(
             centre=1
+        )
+    )
+
+
+@pytest.fixture(
+    name="grid_fit_2"
+)
+def make_grid_fit_2():
+    return db.Fit(
+        id="grid2",
+        unique_tag="grid_fit_2",
+        is_grid_search=True,
+        instance=Gaussian(
+            centre=2
         )
     )
 
@@ -194,20 +209,36 @@ class TestChildren:
         ).fits
         assert len(results) == 6
 
+    def test_query_grid_unique_tag(
+            self,
+            aggregator,
+            grid_fit,
+            grid_fit_2,
+            session
+    ):
+        session.add(
+            grid_fit_2
+        )
+        session.flush()
+
+        grid_agg = aggregator.grid_searches()
+        assert grid_agg[0].unique_tag == "grid_fit_1"
+
+        one = grid_agg.query(
+            grid_agg.search.unique_tag == "grid_fit_1"
+        )
+        assert len(one) == 1
+        assert one[0].unique_tag == "grid_fit_1"
+
     def test_query_before(
             self,
             aggregator,
             grid_fit,
+            grid_fit_2,
             session
     ):
         session.add(
-            db.Fit(
-                id="grid2",
-                is_grid_search=True,
-                instance=Gaussian(
-                    centre=2
-                )
-            )
+            grid_fit_2
         )
         session.flush()
 
