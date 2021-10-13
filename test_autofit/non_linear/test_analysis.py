1aec67e9111a30d0f787e95fa44544b1e25165dc
Richard <richardhayes777@gmail.com>
2021-10-13 12:25:17 +0100

    fixed tests

--- test_autofit/non_linear/test_analysis.py
+++ test_autofit/non_linear/test_analysis.py
@@ -149,7 +149,7 @@ def test_child_paths(
         paths,
         analysis_name="analysis_0"
     )
-    assert sub_paths.output_path == f"{paths.output_path}/analyses/analysis_0"
+    assert sub_paths.output_path == f"{paths.output_path}/analysis_0"
 
 
 @pytest.fixture(
