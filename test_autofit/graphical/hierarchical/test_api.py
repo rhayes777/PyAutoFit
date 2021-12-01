ec8141ac01116e5f8bf1ad8fff6c0a3e808eec63
Richard <richardhayes777@gmail.com>
2021-12-01 09:22:53 +0000

    more tests

--- test_autofit/graphical/hierarchical/test_api.py
+++ test_autofit/graphical/hierarchical/test_api.py
@@ -5,10 +5,17 @@ from autofit import graphical as g
 
 
 @pytest.fixture(
+    name="prior"
+)
+def make_prior():
+    return af.GaussianPrior(100, 10)
+
+
+@pytest.fixture(
     name="hierarchical_factor"
 )
-def make_hierarchical_factor():
-    return g.HierarchicalFactor(
+def make_hierarchical_factor(prior):
+    factor = g.HierarchicalFactor(
         af.GaussianPrior,
         mean=af.GaussianPrior(
             mean=100,
@@ -19,6 +26,10 @@ def make_hierarchical_factor():
             sigma=5
         )
     )
+    factor.add_sampled_variable(
+        prior
+    )
+    return factor
 
 
 def test_priors(
@@ -30,7 +41,13 @@ def test_priors(
 def test_factors(
         hierarchical_factor
 ):
-    hierarchical_factor.add_sampled_variable(
-        af.GaussianPrior(100, 10)
-    )
     assert len(hierarchical_factor.factors) == 1
+
+
+def test_factor(
+        hierarchical_factor,
+        prior
+):
+    factor = hierarchical_factor.factors[0]
+    assert factor.prior_model is hierarchical_factor
+    assert factor.sample_prior is prior
