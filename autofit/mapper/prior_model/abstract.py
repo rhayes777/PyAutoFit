fd70ffd52ed23fdeb0c5a4bf87d26553bc289db1
Richard <richardhayes777@gmail.com>
2022-01-05 10:41:20 +0000

    docs

--- autofit/mapper/prior_model/abstract.py
+++ autofit/mapper/prior_model/abstract.py
@@ -126,7 +126,43 @@ class MeanField:
         )
 
 
-def paths_to_tree(paths, tree=None):
+def paths_to_tree(
+        paths: List[Tuple[str, ...]],
+        tree: Optional[dict] = None
+) -> dict:
+    """
+    Recursively convert a list of paths to a tree structure where common paths
+    are matched.
+
+    Parameters
+    ----------
+    paths
+        A list of paths to attributes in the model.
+    tree
+        A tree already embedded in a parent tree.
+
+    Returns
+    -------
+    A tree with depth max(map(len, paths))
+
+    Examples
+    --------
+    paths_to_tree([
+            ("one", "two", "three"),
+            ("one", "two", "four"),
+    ])
+
+    gives
+    
+    {
+        "one": {
+            "two": {
+                "three": {},
+                "four": {}
+            }
+        }
+    }
+    """
     tree = tree or dict()
     for path in paths:
         if len(path) == 0:
@@ -154,7 +190,11 @@ class AbstractPriorModel(AbstractModel):
         super().__init__()
         self._assertions = list()
 
-    def without_attributes(self):
+    def without_attributes(self) -> "AbstractModel":
+        """
+        Returns a copy of this object with all priors, prior models and
+        constants removed.
+        """
         without_attributes = copy.copy(self)
         for key in self.__dict__:
             if not (key.startswith("_") or key in ("cls", "id")):
@@ -167,7 +207,21 @@ class AbstractPriorModel(AbstractModel):
     def _with_paths(
             self,
             tree: dict
-    ) -> "AbstractPriorModel":
+    ) -> "AbstractModel":
+        """
+        Recursively generate a copy of this model retaining only objects
+        specified by the tree.
+
+        Parameters
+        ----------
+        tree
+            A tree formed of dictionaries describing which components of the
+            model should be retained.
+
+        Returns
+        -------
+        A copy of this model with a subset of attributes
+        """
         if len(tree) == 0:
             return self
 
@@ -195,7 +249,21 @@ class AbstractPriorModel(AbstractModel):
     def with_paths(
             self,
             paths: List[Tuple[str]]
-    ) -> "AbstractPriorModel":
+    ) -> "AbstractModel":
+        """
+        Recursively generate a copy of this model retaining only objects
+        specified by the list of paths.
+
+        Parameters
+        ----------
+        paths
+            A list of tuples of strings each of which points to a retained attribute.
+            All children of a given path are retained.
+
+        Returns
+        -------
+        A copy of this model with a subset of attributes
+        """
         return self._with_paths(
             paths_to_tree(paths)
         )
