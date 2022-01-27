4da5d56745af4b0fd4140d14d4d54b4c817371c6
Richard <richardhayes777@gmail.com>
2022-01-27 13:58:01 +0000

    test illustrating issue is function in class

--- test_autofit/tools/edenise/test_examples/test_local_import.py
+++ test_autofit/tools/edenise/test_examples/test_local_import.py
@@ -25,14 +25,16 @@ def func():
     from VIS_CTI_Autofit.VIS_CTI_NonLinear.VIS_CTI_Grid.VIS_CTI_GridSearch import GridSearch
     from VIS_CTI_Autoconf.VIS_CTI_Tools.decorators import CachedProperty
 
-@property
-def slim(self):
-    """
-    Return a `Grid1D` where the data is stored its `slim` representation, which is an ndarray of shape
-    [total_unmasked_pixels * sub_size, 2].
+class MyClass():
 
-    If it is already stored in its `slim` representation  the `Grid1D` is returned as it is. If not, it is
-    mapped from  `native` to `slim` and returned as a new `Grid1D`.
-    """
-    from VIS_CTI_Autoconf.VIS_CTI_Tools.decorators import CachedProperty
+    @property
+    def slim(self) -> Union[('AbstractGrid1D', 'Grid1D')]:
+        """
+        Return a `Grid1D` where the data is stored its `slim` representation, which is an ndarray of shape
+        [total_unmasked_pixels * sub_size, 2].
+
+        If it is already stored in its `slim` representation  the `Grid1D` is returned as it is. If not, it is
+        mapped from  `native` to `slim` and returned as a new `Grid1D`.
+        """
+        from VIS_CTI_Autoconf.VIS_CTI_Tools.decorators import CachedProperty
 '''
