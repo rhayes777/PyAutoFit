7c022691f3f53cd9fe3eea87a0067d08bb54616a
Richard <richardhayes777@gmail.com>
2022-01-27 14:15:58 +0000

    using recursion to ensure corrections are applied to functions etc. embedded in classes

--- test_autofit/tools/edenise/test_examples/test_local_import.py
+++ test_autofit/tools/edenise/test_examples/test_local_import.py
@@ -4,9 +4,9 @@ from autofit.tools.edenise import File
 
 
 @pytest.fixture(
-    name="file"
+    name="function_file"
 )
-def make_file(
+def make_function_file(
         package,
         examples_directory
 ):
@@ -17,18 +17,38 @@ def make_file(
     )
 
 
-def test(file):
-    print(file.target_string)
-    assert file.target_string == '''
+def test_function(function_file):
+    print(function_file.target_string)
+    assert function_file.target_string == '''
 
 def func():
     from VIS_CTI_Autofit.VIS_CTI_NonLinear.VIS_CTI_Grid.VIS_CTI_GridSearch import GridSearch
     from VIS_CTI_Autoconf.VIS_CTI_Tools.decorators import CachedProperty
+'''
+
+
+@pytest.fixture(
+    name="class_file"
+)
+def make_class_file(
+        package,
+        examples_directory
+):
+    return File(
+        examples_directory / "class_import.py",
+        parent=package,
+        prefix=""
+    )
+
+
+def test_class(class_file):
+    print(class_file.target_string)
+    assert class_file.target_string == '''
 
 class MyClass():
 
     @property
-    def slim(self) -> Union[('AbstractGrid1D', 'Grid1D')]:
+    def slim(self):
         """
         Return a `Grid1D` where the data is stored its `slim` representation, which is an ndarray of shape
         [total_unmasked_pixels * sub_size, 2].
