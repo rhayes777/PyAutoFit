5772e2082fee4bf74c0ffe44c72afbc9da2131c5
Richard <richardhayes777@gmail.com>
2022-02-04 13:51:41 +0000

    fail with useful message when attempting to use database

--- autofit/database/sqlalchemy_.py
+++ autofit/database/sqlalchemy_.py
@@ -12,6 +12,28 @@ except ImportError:
         def __mro_entries__(self, *args, **kwargs):
             return tuple()
 
+        def declarative_base(self):
+            return MockBase
+
+        def __getitem__(self, item):
+            fail()
+
+        def __setitem__(self, key, value):
+            fail()
+
 
     sa = MockSQlAlchemy()
-    declarative = MockSQlAlchemy()
+    declarative = sa
+
+
+    def fail():
+        raise ImportError(
+            "Please install SQLAlchemy to use the database"
+        )
+
+
+    class MockBase:
+        def __init__(self, *args, **kwargs):
+            fail()
+
+        metadata = sa
