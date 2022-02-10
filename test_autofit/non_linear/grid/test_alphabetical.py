4586829a0a46e6f81a3e3cbb4bb1e140259be184
Richard <richardhayes777@gmail.com>
2022-02-10 11:02:15 +0000

    check against collection

--- test_autofit/non_linear/grid/test_alphabetical.py
+++ test_autofit/non_linear/grid/test_alphabetical.py
@@ -1,4 +1,37 @@
+import pytest
+
 import autofit as af
+
+
+@pytest.fixture(
+    name="prior"
+)
+def make_prior():
+    return af.UniformPrior()
+
+
+@pytest.fixture(
+    name="model"
+)
+def make_model(prior):
+    model = af.Model(
+        af.Gaussian
+    )
+    model.centre = prior
+    return model
+
+
+@pytest.fixture(
+    name="collection"
+)
+def make_collection(
+        prior,
+        model
+):
+    return af.Collection([
+        prior,
+        model
+    ])
 
 
 def test_jump_id():
@@ -20,3 +53,13 @@ def test_alphabetise_model():
 
     model.alphabetise()
     assert model.priors_ordered_by_id[0] is model.centre
+
+
+def test_alphabetise_collection(
+        collection,
+        prior,
+        model,
+):
+    collection.alphabetise()
+    assert collection[0] == prior
+    assert collection[1] == model
