5d8c5a1b5a1f93f1ade912c388a245b2590b610d
Richard <richardhayes777@gmail.com>
2022-03-23 11:43:52 +0000

    test various values

--- test_autofit/mapper/prior/test_log_gaussian.py
+++ test_autofit/mapper/prior/test_log_gaussian.py
@@ -1,32 +1,29 @@
-import numpy as np
-from matplotlib import pyplot as plt
+import pytest
 
 import autofit as af
-from autofit.messages import LogNormalMessage
 
 
-def test():
-    # message = LogNormalMessage(
-    #     mean=1.0,
-    #     sigma=2.0
-    # )
-    # print(message)
-    # x = np.linspace(
-    #     start=0,
-    #     stop=100,
-    #     num=100,
-    # )
-    #
-    # y = list(map(
-    #     message.pdf,
-    #     x
-    # ))
-    #
-    # plt.plot(x, y)
-    # plt.show()
-
-    log_gaussian = af.LogGaussianPrior(
+@pytest.fixture(
+    name="log_gaussian"
+)
+def make_log_gaussian():
+    return af.LogGaussianPrior(
         mean=1.0,
         sigma=2.0
     )
-    assert log_gaussian.value_for(0.0) == 0.0
+
+
+@pytest.mark.parametrize(
+    "unit, value",
+    [
+        (0.0, 0.0),
+        (0.1, 0.2),
+        (0.5, 2.7),
+        (0.9, 35),
+    ]
+)
+def test_values(
+        log_gaussian,
+        unit, value
+):
+    assert log_gaussian.value_for(unit) == pytest.approx(value, rel=0.1)
