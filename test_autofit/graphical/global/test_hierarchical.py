c248bea0574e7826f40687ba373189e7de5ee472
Richard <richardhayes777@gmail.com>
2022-03-23 16:11:23 +0000

    illustration that altering hierarchical factor changes overall likelihood

--- test_autofit/graphical/global/test_hierarchical.py
+++ test_autofit/graphical/global/test_hierarchical.py
@@ -37,8 +37,15 @@ def make_graph(
     )
 
 
-def test_info(graph):
-    assert graph.global_prior_model.info == """PriorFactors
+@pytest.fixture(
+    name="model"
+)
+def make_model(graph):
+    return graph.global_prior_model
+
+
+def test_info(model):
+    assert model.info == """PriorFactors
 
 PriorFactor0 (HierarchicalFactor0)                                                        GaussianPrior, mean = 1.0, sigma = 0.01
 PriorFactor1 (HierarchicalFactor0)                                                        GaussianPrior, mean = 0.5, sigma = 0.1
@@ -68,8 +75,7 @@ AnalysisFactor0.one, PriorFactor3                                               
 PriorFactor2                                                                              UniformPrior, lower_limit = 0.0, upper_limit = 1.0"""
 
 
-def test_instance(graph):
-    model = graph.global_prior_model
+def test_instance(model):
     assert model.prior_count == 4
     instance = model.instance_from_unit_vector(
         [0.1, 0.2, 0.3, 0.4]
@@ -89,3 +95,28 @@ def test_instance(graph):
     assert instance[1].drawn_prior == 0.2
 
     assert dist_model_1 == dist_model_2
+
+
+@pytest.mark.parametrize(
+    "unit_vector, likelihood",
+    [
+        ([0.1, 0.2, 0.3, 0.5], -2.248),
+        ([0.1, 0.2, 0.4, 0.8], -2.280),
+        ([0.1, 0.2, 0.3, 0.3], -2.239),
+    ]
+)
+def test_likelihood(
+        graph,
+        model,
+        unit_vector,
+        likelihood,
+):
+    instance = model.instance_from_unit_vector(
+        unit_vector
+    )
+    assert graph.log_likelihood_function(
+        instance
+    ) == pytest.approx(
+        likelihood,
+        rel=0.001
+    )
