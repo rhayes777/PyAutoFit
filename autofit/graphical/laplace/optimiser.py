1796a7efb9fb7cf23ba5c0e61088c4c85ca6dc94
Matthew Griffiths <matthew@concr.co>
2022-04-20 14:31:41 +0100

    setting newton_abs_direction to default search direction algorithm

--- autofit/graphical/laplace/optimiser.py
+++ autofit/graphical/laplace/optimiser.py
@@ -19,7 +19,7 @@ class LaplaceOptimiser(AbstractFactorOptimiser):
     def __init__(
             self,
             make_hessian=make_posdef_hessian,
-            search_direction=newton.newton_direction,
+            search_direction=newton.newton_abs_direction,
             calc_line_search=newton.line_search,
             quasi_newton_update=newton.full_diag_update,
             stop_conditions=newton.stop_conditions,
