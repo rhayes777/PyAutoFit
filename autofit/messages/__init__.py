fd89313e2bdb7356b6bc4fc177e698d6de7c9a9f
Matthew Griffiths <matthew@concr.co>
2022-05-05 10:07:19 +0100

    adding NaturalNormal to autofit.messages.__init__

--- autofit/messages/__init__.py
+++ autofit/messages/__init__.py
@@ -4,6 +4,7 @@ from .fixed import FixedMessage
 from .gamma import GammaMessage
 from .normal import (
     NormalMessage,
+    NaturalNormal, 
     UniformNormalMessage,
     LogNormalMessage,
     MultiLogitNormalMessage,
