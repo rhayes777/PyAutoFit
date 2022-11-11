63ecb554c480285d63075e97f91955a86d4aaefd
Richard <richardhayes777@gmail.com>
2022-11-11 10:39:06 +0000

    test illustrating old and new logpdf_gradients match

--- test_autofit/test_correspondence.py
+++ test_autofit/test_correspondence.py
@@ -1,9 +1,15 @@
-from autofit.messages import UniformNormalMessage
+from autofit.messages import UniformNormalMessage, NormalMessage
+from autofit.messages.transform import phi_transform
+
+OldUniformNormalMessage = NormalMessage.transformed(
+    phi_transform, "UniformNormalMessage"
+)
 
 
 def test_logpdf_gradient():
     message = UniformNormalMessage(1.0, 0.5)
     x = message.sample()
 
-    res = message.logpdf_gradient(x)
-    print(res)
+    old_message = OldUniformNormalMessage(1.0, 0.5)
+
+    assert message.logpdf_gradient(x) == old_message.logpdf_gradient(x)
