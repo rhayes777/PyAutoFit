68a0c91686948fc75823a136c13b9aca0606da30
Richard <richardhayes777@gmail.com>
2022-11-14 12:21:05 +0000

    fixed a trest

--- test_autofit/graphical/regression/test_pickling.py
+++ test_autofit/graphical/regression/test_pickling.py
@@ -6,22 +6,12 @@ from autofit.messages.transform_wrapper import TransformedWrapperInstance
 
 
 def test_pickle_log_10_transform():
-    loaded = pickle.loads(
-        pickle.dumps(
-            log_10_transform
-        )
-    )
+    loaded = pickle.loads(pickle.dumps(log_10_transform))
     assert loaded.transform(10) == 1
 
 
 def test_copy_and_pickle():
-    original = UniformNormalMessage(
-        mean=1.0,
-        sigma=2.0,
-    )
+    original = UniformNormalMessage(mean=1.0, sigma=2.0,)
     copied = original.copy()
-    assert isinstance(
-        copied,
-        TransformedWrapperInstance
-    )
+    assert copied == original
     assert copied is not original
