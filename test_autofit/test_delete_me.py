784415c2c4645728b9c7baccc5b679308dae4d5b
Richard <richardhayes777@gmail.com>
2022-11-14 15:06:18 +0000

    more specific test

--- test_autofit/test_delete_me.py
+++ test_autofit/test_delete_me.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 import autofit as af
 from autofit import DiagonalMatrix
 
@@ -7,5 +5,4 @@ from autofit import DiagonalMatrix
 def test_from_mode():
     message = af.UniformPrior(lower_limit=10, upper_limit=20).message
     mean = message.from_mode(14.03, covariance=DiagonalMatrix(0.48)).mean
-    print(mean)
-    assert not np.isnan(mean)
+    assert mean == 14.03
