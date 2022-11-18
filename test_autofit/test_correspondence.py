cc6fa6fb94427d97f9a6d8f87ba2cb0ae246a8bc
Richard <richardhayes777@gmail.com>
2022-11-18 14:45:23 +0000

    updated correspondence tests to match explicit values

--- test_autofit/test_correspondence.py
+++ test_autofit/test_correspondence.py
@@ -7,10 +7,6 @@ from autofit.messages import NormalMessage
 from autofit.messages.composed_transform import TransformedMessage
 from autofit.messages.transform import phi_transform, LinearShiftTransform
 
-OldUniformNormalMessage = NormalMessage.transformed(
-    phi_transform, "UniformNormalMessage"
-)
-
 
 @pytest.fixture(name="message")
 def make_message():
@@ -22,46 +18,44 @@ def make_x(message):
     return 0.5
 
 
-@pytest.fixture(name="old_message")
-def make_old_message():
-    return OldUniformNormalMessage.shifted(shift=10.0, scale=20.0)(0, 1)
-
-
-def test_logpdf_gradient(message, old_message):
+def test_logpdf_gradient(message):
     x = 15
-    assert message.logpdf_gradient(x) == old_message.logpdf_gradient(x)
+    a, b = message.logpdf_gradient(x)
+    assert a == -1.146406744764459
+    assert float(b) == pytest.approx(0.10612641)
 
 
-def test_log_pdf(message, old_message):
+def test_log_pdf(message):
     x = 15
-    assert message.logpdf(x) == old_message.logpdf(x)
+    assert message.logpdf(x) == -1.146406744764459
 
 
-def test_logpdf_gradient_hessian(message, old_message):
+def test_logpdf_gradient_hessian(message):
     x = 15
-    assert message.logpdf_gradient_hessian(x) == old_message.logpdf_gradient_hessian(x)
+    assert message.logpdf_gradient_hessian(x) == (
+        -1.146406744764459,
+        0.1061263938950674,
+        -0.0361932706027801,
+    )
 
 
-def test_calc_log_base_measure(message, old_message, x):
-    assert message.calc_log_base_measure(x) == old_message.calc_log_base_measure(x)
+def test_calc_log_base_measure(message, x):
+    assert message.calc_log_base_measure(x) == -0.9189385332046727
 
 
-def test_to_canonical_form(message, old_message):
+def test_to_canonical_form(message):
     x = 15
-    assert (message.to_canonical_form(x) == old_message.to_canonical_form(x)).all()
+    assert np.allclose(
+        message.to_canonical_form(x), np.array([-0.67448975, 0.45493642])
+    )
 
 
-def test_factor(message, old_message):
+def test_factor(message):
     x = 15
-    assert message.factor(x) == old_message.factor(x)
+    assert message.factor(x) == -4.14213901831845
 
 
-# TODO: commented because old variance calculation was apparently broken
-# def test_variance(message, old_message):
-#     assert message.variance == old_message.variance
-
-
-def test_value_for(message, old_message, x):
+def test_value_for(message, x):
     assert message.value_for(x) == 20
 
 
@@ -82,12 +76,9 @@ def test_transform_variance():
 def test_transform_uniform():
     normal = NormalMessage(0, 1)
 
-    print(TransformedMessage(normal, phi_transform,).variance)
-
     transformed = TransformedMessage(
         normal, phi_transform, LinearShiftTransform(shift=10.0, scale=20.0),
     )
-    print(transformed.variance)
     assert transformed.variance
 
 
@@ -95,16 +86,6 @@ def test_from_mode():
     message = UniformPrior(lower_limit=10, upper_limit=20).message
     mean = message.from_mode(14.03, covariance=np.zeros(())).mean
     assert mean == 14.03
-
-
-def test_log_pdf_gradient_old(old_message):
-    logl, _ = old_message.logpdf_gradient(15)
-    assert logl == pytest.approx(-1.1464067)
-
-
-def test_numerical_logpdf_gradient_old(old_message):
-    logl, _ = old_message.numerical_logpdf_gradient(15)
-    assert logl == pytest.approx(-1.1464067)
 
 
 def test_numerical_logpdf_gradient(message):
@@ -115,17 +96,6 @@ def test_numerical_logpdf_gradient(message):
 def test_log_pdf_gradient(message):
     logl, _ = message.logpdf_gradient(15)
     assert logl == pytest.approx(-1.1464067)
-
-
-def test_regression_old():
-    message = OldUniformNormalMessage.shifted(shift=0.3, scale=0.8)(0, 1)
-    x = 1.076
-
-    log_likelihood, gradient = message.logpdf_gradient(x)
-    numerical_log_likelihood, numerical_gradient = message.numerical_logpdf_gradient(x)
-
-    assert log_likelihood == numerical_log_likelihood
-    assert float(gradient) == pytest.approx(numerical_gradient, rel=0.001)
 
 
 def test_regression():
@@ -142,15 +112,3 @@ def test_regression():
     assert log_likelihood == numerical_log_likelihood
     assert float(gradient) == pytest.approx(numerical_gradient, rel=0.001)
     assert float(approx_gradient) == pytest.approx(numerical_gradient, rel=0.001)
-
-
-# from_natural_parameters
-# check_support
-# project
-# invert_natural_parameters
-# cdf
-# invert_sufficient_statistics
-# _sample
-# from_mode
-# update_invalid
-# log_base_measure
