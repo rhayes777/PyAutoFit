9830e2c768f7c1de278bcb341f7545f36305667c
Richard <richardhayes777@gmail.com>
2022-12-19 12:10:08 +0000

    ensuring modify model is called elsewhere

--- autofit/non_linear/analysis/free_parameter.py
+++ autofit/non_linear/analysis/free_parameter.py
@@ -8,17 +8,11 @@ from autofit.mapper.prior_model.collection import CollectionPriorModel
 from .analysis import Analysis
 from .indexed import IndexCollectionAnalysis
 
-logger = logging.getLogger(
-    __name__
-)
+logger = logging.getLogger(__name__)
 
 
 class FreeParameterAnalysis(IndexCollectionAnalysis):
-    def __init__(
-            self,
-            *analyses: Analysis,
-            free_parameters: Tuple[Prior, ...]
-    ):
+    def __init__(self, *analyses: Analysis, free_parameters: Tuple[Prior, ...]):
         """
         A combined analysis with free parameters.
 
@@ -35,30 +29,17 @@ class FreeParameterAnalysis(IndexCollectionAnalysis):
         """
         super().__init__(*analyses)
         self.free_parameters = [
-            parameter for parameter
-            in free_parameters
-            if isinstance(
-                parameter,
-                Prior
-            )
+            parameter for parameter in free_parameters if isinstance(parameter, Prior)
         ]
         # noinspection PyUnresolvedReferences
         self.free_parameters += [
             prior
-            for parameter
-            in free_parameters
-            if isinstance(
-                parameter,
-                (AbstractPriorModel, TuplePrior)
-            )
-            for prior
-            in parameter.priors
+            for parameter in free_parameters
+            if isinstance(parameter, (AbstractPriorModel, TuplePrior))
+            for prior in parameter.priors
         ]
 
-    def modify_model(
-            self,
-            model: AbstractPriorModel
-    ) -> AbstractPriorModel:
+    def modify_model(self, model: AbstractPriorModel) -> AbstractPriorModel:
         """
         Create prior models where free parameters are replaced with new
         priors. Return those prior models as a collection.
@@ -77,10 +58,16 @@ class FreeParameterAnalysis(IndexCollectionAnalysis):
         A new model with all the same priors except for those associated
         with free parameters.
         """
-        return CollectionPriorModel([
-            model.mapper_from_partial_prior_arguments({
-                free_parameter: free_parameter.new()
-                for free_parameter in self.free_parameters
-            })
-            for _ in self.analyses
-        ])
+        return CollectionPriorModel(
+            [
+                analysis.modify_model(
+                    model.mapper_from_partial_prior_arguments(
+                        {
+                            free_parameter: free_parameter.new()
+                            for free_parameter in self.free_parameters
+                        }
+                    )
+                )
+                for analysis in self.analyses
+            ]
+        )
