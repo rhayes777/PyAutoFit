16bac1b879fac109f586e890526b7c02d6076a92
Richard <richardhayes777@gmail.com>
2022-12-19 14:09:13 +0000

    include 0 dimension models when using has

--- test_autofit/mapper/model/test_has.py
+++ test_autofit/mapper/model/test_has.py
@@ -29,3 +29,12 @@ def test_collection_of_collection(collection):
     collection = af.Collection(collection=collection)
     assert collection.has_instance(af.Prior)
     assert collection.has_model(af.Gaussian)
+
+
+def test_has_0_dimension():
+    collection = af.Collection(
+        gaussian=af.Model(af.Gaussian, centre=0.0, normalization=0.1, sigma=0.01,)
+    )
+
+    assert not collection.has_model(af.Gaussian)
+    assert collection.has(af.Gaussian)
