5b6c27a21cb330e967a6de9be5cdec3b1fe686ad
Richard <richardhayes777@gmail.com>
2022-12-19 11:30:21 +0000

    test existing methods

--- test_autofit/mapper/model/test_has.py
+++ test_autofit/mapper/model/test_has.py
@@ -8,11 +8,24 @@ def make_prior_model():
     return af.Model(af.Gaussian)
 
 
+@pytest.fixture(name="collection")
+def make_collection(model):
+    return af.Collection(gaussian=model)
+
+
 def test_model_has(model):
-    assert model.has(af.Prior)
+    assert model.has_instance(af.Prior)
 
 
-def test_collection_has(model):
-    collection = af.Collection(gaussian=model)
+def test_collection_has(collection):
+    assert collection.has_instance(af.Prior)
 
-    assert collection.has(af.Prior)
+
+def test_collection_has_model(collection):
+    assert collection.has_model(af.Gaussian)
+
+
+def test_collection_of_collection(collection):
+    collection = af.Collection(collection=collection)
+    assert collection.has_instance(af.Prior)
+    assert collection.has_model(af.Gaussian)
