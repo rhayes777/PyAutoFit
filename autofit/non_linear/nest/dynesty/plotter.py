1e0f09efa07ff03b427cf25e5a298e7521d9dd35
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-12-20 10:37:04 +0000

    docs

--- autofit/non_linear/nest/dynesty/plotter.py
+++ autofit/non_linear/nest/dynesty/plotter.py
@@ -10,6 +10,19 @@ class DynestyPlotter(SamplesPlotter):
     
     @staticmethod
     def log_plot_exception(plot_name : str):
+        """
+        Plotting the results of a ``dynesty`` model-fit before they have converged on an
+        accurate estimate of the posterior can lead the ``dynesty`` plotting routines
+        to raise a ``ValueError``.
+
+        This exception is caught in each of the plotting methods below, and this
+        function is used to log the behaviour.
+
+        Parameters
+        ----------
+        plot_name
+            The name of the ``dynesty`` plot which raised a ``ValueError``
+        """
 
         logger.info(
             f"Dynesty unable to produce {plot_name} visual: posterior estimate therefore"
@@ -19,6 +32,14 @@ class DynestyPlotter(SamplesPlotter):
 
     @skip_plot_in_test_mode
     def boundplot(self, **kwargs):
+        """
+        Plots the in-built ``dynesty`` plot ``boundplot``.
+        
+        This figure plots the bounding distribution used to propose either (1) live points
+        at a given iteration or (2) a specific dead point during
+        the course of a run, projected onto the two dimensions specified
+        by `dims`.
+        """
 
         dyplot.boundplot(
             results=self.samples.results_internal,
@@ -31,7 +52,14 @@ class DynestyPlotter(SamplesPlotter):
 
     @skip_plot_in_test_mode
     def cornerbound(self, **kwargs):
+        """
+        Plots the in-built ``dynesty`` plot ``cornerbound``.
 
+        This figure plots the bounding distribution used to propose either (1) live points
+        at a given iteration or (2) a specific dead point during
+        the course of a run, projected onto all pairs of dimensions.
+        """
+        
         dyplot.cornerbound(
             results=self.samples.results_internal,
             labels=self.model.parameter_labels_with_superscripts_latex,
@@ -43,7 +71,11 @@ class DynestyPlotter(SamplesPlotter):
 
     @skip_plot_in_test_mode
     def cornerplot(self, **kwargs):
+        """
+        Plots the in-built ``dynesty`` plot ``cornerplot``.
 
+        This figure plots a corner plot of the 1-D and 2-D marginalized posteriors.
+        """
         try:
 
             dyplot.cornerplot(
@@ -61,7 +93,11 @@ class DynestyPlotter(SamplesPlotter):
 
     @skip_plot_in_test_mode
     def cornerpoints(self, **kwargs):
+        """
+        Plots the in-built ``dynesty`` plot ``cornerpoints``.
 
+        This figure plots a (sub-)corner plot of (weighted) samples.
+        """
         try:
             dyplot.cornerpoints(
                 results=self.samples.results_internal,
@@ -78,7 +114,12 @@ class DynestyPlotter(SamplesPlotter):
 
     @skip_plot_in_test_mode
     def runplot(self, **kwargs):
+        """
+        Plots the in-built ``dynesty`` plot ``runplot``.
 
+        This figure plots live points, ln(likelihood), ln(weight), and ln(evidence)
+        as a function of ln(prior volume).
+        """
         try:
             dyplot.runplot(
                 results=self.samples.results_internal,
@@ -94,7 +135,11 @@ class DynestyPlotter(SamplesPlotter):
 
     @skip_plot_in_test_mode
     def traceplot(self, **kwargs):
+        """
+        Plots the in-built ``dynesty`` plot ``traceplot``.
 
+        This figure plots traces and marginalized posteriors for each parameter.
+        """
         try:
 
             dyplot.traceplot(
