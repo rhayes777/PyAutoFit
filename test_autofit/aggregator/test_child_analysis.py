cafccf61552397ff3d8cc618a9007972fe4e4d8f
Richard <richard@rghsoftware.co.uk>
2023-05-10 14:21:44 +0100

    child_analysis_values method

--- test_autofit/aggregator/test_child_analysis.py
+++ test_autofit/aggregator/test_child_analysis.py
@@ -3,12 +3,21 @@ from pathlib import Path
 import pytest
 
 from autofit import SearchOutput
+from autofit.aggregator import Aggregator
+
+
+@pytest.fixture(name="directory")
+def make_directory():
+    return Path(__file__).parent
+
+
+@pytest.fixture(name="search_output")
+def make_search_output(directory):
+    return SearchOutput(directory / "search_output")
 
 
 @pytest.fixture(name="child_analyses")
-def make_child_analyses():
-    directory = Path(__file__).parent / "search_output"
-    search_output = SearchOutput(str(directory))
+def make_child_analyses(search_output):
     return search_output.child_analyses
 
 
@@ -18,3 +27,11 @@ def test_child_analysis(child_analyses):
 
 def test_child_analysis_pickles(child_analyses):
     assert child_analyses[0].example == "hello world"
+
+
+def test_child_analysis_values(directory):
+    aggregator = Aggregator(directory)
+
+    assert list(aggregator.child_analysis_values("example")) == [
+        ["hello world", "hello world"]
+    ]
