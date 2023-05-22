6d206613e65ddada58e0b444d7229d1d30dea16f
Richard <richard@rghsoftware.co.uk>
2023-05-22 12:30:38 +0100

    extra tests

--- test_autofit/interpolator/test_linear_analysis.py
+++ test_autofit/interpolator/test_linear_analysis.py
@@ -1,28 +1,43 @@
 import numpy as np
+import pytest
 
 from autofit.interpolator.covariance import LinearAnalysis, LinearRelationship
 
 
-def test_trivial_linear_analysis():
+@pytest.mark.parametrize(
+    "m, c, y",
+    [
+        (2.0, 0.0, [2.0, 4.0, 6.0]),
+        (1.0, 0.0, [1.0, 2.0, 3.0]),
+        (1.0, 1.0, [2.0, 3.0, 4.0]),
+    ],
+)
+def test_trivial_linear_analysis(m, c, y):
     linear_analysis = LinearAnalysis(
         x=[1.0, 2.0, 3.0],
-        y=[2.0, 4.0, 6.0],
+        y=y,
         inverse_covariance_matrix=np.eye(3),
     )
     instance = [
         LinearRelationship(
-            m=2.0,
-            c=0.0,
+            m=m,
+            c=c,
         )
     ]
-    assert list(linear_analysis._y(instance)) == [2.0, 4.0, 6.0]
+    assert list(linear_analysis._y(instance)) == y
     assert linear_analysis.log_likelihood_function(instance) == 0.0
 
 
-def test_multiple_time_points_linear_analysis():
-    y = [2.0, 3.0, 4.0, 6.0, 6.0, 9.0]
+@pytest.mark.parametrize(
+    "x, y",
+    [
+        ([1.0, 1.0, 2.0, 2.0, 3.0, 3.0], [2.0, 3.0, 4.0, 6.0, 6.0, 9.0]),
+        ([2.0, 2.0, 1.0, 1.0, 3.0, 3.0], [4.0, 6.0, 2.0, 3.0, 6.0, 9.0]),
+    ],
+)
+def test_multiple_time_points_linear_analysis(x, y):
     linear_analysis = LinearAnalysis(
-        x=[1.0, 1.0, 2.0, 2.0, 3.0, 3.0],
+        x=x,
         y=y,
         inverse_covariance_matrix=np.eye(6),
     )
@@ -36,5 +51,5 @@ def test_multiple_time_points_linear_analysis():
             c=0.0,
         ),
     ]
-    assert list(linear_analysis._y(instance)) == y
+    assert list(linear_analysis._y(instance)) == [2.0, 3.0, 4.0, 6.0, 6.0, 9.0]
     assert linear_analysis.log_likelihood_function(instance) == 0.0
