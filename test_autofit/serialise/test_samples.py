e3081139b01df7fd9b7b1a9815031a79b6be5ea6
Richard <richard@rghsoftware.co.uk>
2023-06-26 11:03:21 +0100

    ensure keys are added for the case of dictionaries when calling to_dict

--- test_autofit/serialise/test_samples.py
+++ test_autofit/serialise/test_samples.py
@@ -53,31 +53,31 @@ def test_summary(summary, model, sample):
 def make_summary_dict():
     return {
         "type": "autofit.non_linear.samples.summary.SamplesSummary",
+        "max_log_likelihood_sample": {
+            "type": "autofit.non_linear.samples.sample.Sample",
+            "arguments": {
+                "log_likelihood": 4.0,
+                "kwargs": {"centre": 2.0, "normalization": 4.0, "sigma": 6.0},
+                "weight": 6.0,
+                "log_prior": 5.0,
+            },
+        },
+        "model": {
+            "class_path": "autofit.example.model.Gaussian",
+            "type": "model",
+            "centre": {"lower_limit": 0.0, "upper_limit": 1.0, "type": "Uniform"},
+            "normalization": {
+                "lower_limit": 0.0,
+                "upper_limit": 1.0,
+                "type": "Uniform",
+            },
+            "sigma": {"lower_limit": 0.0, "upper_limit": 1.0, "type": "Uniform"},
+        },
         "covariance_matrix": [
             [2.0, 3.0, 3.9999999999999996],
             [3.0, 4.5, 6.0],
             [4.0, 6.0, 7.999999999999999],
         ],
-        "max_log_likelihood_sample": {
-            "arguments": {
-                "kwargs": {"arguments": {}, "type": "dict"},
-                "log_likelihood": 4.0,
-                "log_prior": 5.0,
-                "weight": 6.0,
-            },
-            "type": "autofit.non_linear.samples.sample.Sample",
-        },
-        "model": {
-            "centre": {"lower_limit": 0.0, "type": "Uniform", "upper_limit": 1.0},
-            "class_path": "autofit.example.model.Gaussian",
-            "normalization": {
-                "lower_limit": 0.0,
-                "type": "Uniform",
-                "upper_limit": 1.0,
-            },
-            "sigma": {"lower_limit": 0.0, "type": "Uniform", "upper_limit": 1.0},
-            "type": "model",
-        },
     }
 
 
@@ -91,6 +91,8 @@ def test_from_dict(summary_dict):
     assert isinstance(summary.model, af.Model)
     assert isinstance(summary.max_log_likelihood_sample, af.Sample)
     assert isinstance(summary.covariance_matrix(), np.ndarray)
+
+    assert isinstance(summary.max_log_likelihood(), af.Gaussian)
 
 
 def test_generic_from_dict(summary_dict):
