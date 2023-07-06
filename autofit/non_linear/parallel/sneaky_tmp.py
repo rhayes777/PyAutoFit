2387fdd558ec785ef91d603145f43d19e3f69850
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2023-07-06 22:33:38 +0200

    added mpi check + remaining methods copied over from dynesty

--- autofit/non_linear/parallel/sneaky_tmp.py
+++ autofit/non_linear/parallel/sneaky_tmp.py
@@ -62,7 +62,7 @@ class SneakyPool:
     def __init__(
             self,
             processes: int,
-            fitness,
+            fitness: Callable,
             prior_transform: Optional[Callable] = None,
             fitness_args: Optional[Iterable] = None,
             fitness_kwargs: Optional[dict] = None,
@@ -80,3 +80,82 @@ class SneakyPool:
         self.prior_transform_kwargs = prior_transform_kwargs or {}
         self.processes = processes
         self.pool = None
+        self.comm = MPI.COMM_WORLD
+
+    def check_if_mpi(self):
+
+        if self.comm.Get_size() > 1:
+            return True
+        else:
+            return False
+
+    def __enter__(self):
+        """
+        Activate the mp / mpi pool
+        """
+        
+        init_args = (
+            self.fitness_init, self.prior_transform_init,
+            self.fitness_args, self.fitness_kwargs,
+            self.prior_transform_args, self.prior_transform_kwargs
+        )
+
+        use_mpi = self.check_if_mpi()
+
+        if use_mpi:
+            self.pool = MPIPoolExecutor(
+                max_workers=self.processes,
+                initializer=initializer,
+                initargs=init_args
+            )
+        else:
+            self.pool = mp.Pool(
+                processes=self.processes,
+                initializer=initializer,
+                initargs=init_args
+            )
+        
+        initializer(*init_args)
+
+        return self
+
+    def map(
+            self, function: Callable,
+            iterable: Iterable
+    ):
+        """
+        Map a function over an iterable using the map method
+        of the initialized pool.
+
+        Parameters
+        ----------
+        function
+            A function to map
+        iterable
+            An iterable to map over
+
+        """
+        return self.pool.map(function, iterable)
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.pool.terminate()
+        except:  # noqa
+            pass
+        try:
+            del (FunctionCache.fitness, FunctionCache.prior_transform,
+                 FunctionCache.fitness_args, FunctionCache.fitness_kwargs,
+                 FunctionCache.prior_transform_args, FunctionCache.prior_transform_kwargs)
+        except:  # noqa
+            pass
+    
+    @property
+    def size(self):
+
+        return self.njobs
+    
+    def close(self):
+        self.pool.close()
+    
+    def join(self):
+        self.pool.join()
