cf3d1e2cdc59ebdc65a8e004142404e0b9168787
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-07-21 09:18:54 +0100

    clean dynesty tesst

--- test_autofit/non_linear/search/nest/test_dynesty.py
+++ test_autofit/non_linear/search/nest/test_dynesty.py
@@ -22,47 +22,46 @@ class MockDynestySampler:
         self.results = results
 
 
-class TestDynestyConfig:
+def test__loads_from_config_file_if_not_input():
+    
+    search = af.DynestyStatic(
+        nlive=151,
+        dlogz=0.1,
+        iterations_per_update=501,
+        number_of_cores=2,
+    )
 
-    def test__loads_from_config_file_if_not_input(self):
-        dynesty = af.DynestyStatic(
-            nlive=151,
-            dlogz=0.1,
-            iterations_per_update=501,
-            number_of_cores=2,
-        )
+    assert search.iterations_per_update == 501
 
-        assert dynesty.iterations_per_update == 501
+    assert search.config_dict_search["nlive"] == 151
+    assert search.config_dict_run["dlogz"] == 0.1
+    assert search.number_of_cores == 2
 
-        assert dynesty.config_dict_search["nlive"] == 151
-        assert dynesty.config_dict_run["dlogz"] == 0.1
-        assert dynesty.number_of_cores == 2
+    search = af.DynestyStatic()
 
-        dynesty = af.DynestyStatic()
+    assert search.iterations_per_update == 500
 
-        assert dynesty.iterations_per_update == 500
+    assert search.config_dict_search["nlive"] == 150
+    assert search.config_dict_run["dlogz"] == None
+    assert search.number_of_cores == 1
 
-        assert dynesty.config_dict_search["nlive"] == 150
-        assert dynesty.config_dict_run["dlogz"] == None
-        assert dynesty.number_of_cores == 1
+    search = af.DynestyDynamic(
+        facc=0.4,
+        iterations_per_update=501,
+        dlogz_init=0.2,
+        number_of_cores=3,
+    )
 
-        dynesty = af.DynestyDynamic(
-            facc=0.4,
-            iterations_per_update=501,
-            dlogz_init=0.2,
-            number_of_cores=3,
-        )
+    assert search.iterations_per_update == 501
 
-        assert dynesty.iterations_per_update == 501
+    assert search.config_dict_search["facc"] == 0.4
+    assert search.config_dict_run["dlogz_init"] == 0.2
+    assert search.number_of_cores == 3
 
-        assert dynesty.config_dict_search["facc"] == 0.4
-        assert dynesty.config_dict_run["dlogz_init"] == 0.2
-        assert dynesty.number_of_cores == 3
+    search = af.DynestyDynamic()
 
-        dynesty = af.DynestyDynamic()
+    assert search.iterations_per_update == 501
 
-        assert dynesty.iterations_per_update == 501
-
-        assert dynesty.config_dict_search["facc"] == 0.6
-        assert dynesty.config_dict_run["dlogz_init"] == 0.01
-        assert dynesty.number_of_cores == 4
+    assert search.config_dict_search["facc"] == 0.6
+    assert search.config_dict_run["dlogz_init"] == 0.01
+    assert search.number_of_cores == 4
