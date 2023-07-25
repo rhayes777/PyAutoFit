3209af7a4c8b46e59b26abd8e504d3b5ef89e2d6
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-07-25 18:39:26 +0100

    remove duplicate search.json file

--- autofit/non_linear/paths/directory.py
+++ autofit/non_linear/paths/directory.py
@@ -242,7 +242,6 @@ class DirectoryPaths(AbstractPaths):
 
         self.save_identifier()
         self.save_parent_identifier()
-        self._save_search(config_dict=search_config_dict)
         self._save_model_info(model=self.model)
         self._save_parameter_names_file(model=self.model)
         if info:
