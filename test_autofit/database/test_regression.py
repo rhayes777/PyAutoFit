7aa832e26d288f0e8c1da3724aa7cd62b797ed57
Richard <richard@rghsoftware.co.uk>
2023-08-25 09:07:35 +0100

    extra test asserting instantiation

--- test_autofit/database/test_regression.py
+++ test_autofit/database/test_regression.py
@@ -1,18 +1,23 @@
+import pytest
+
 import autofit as af
 from autofit import database as db
 
 
-def test_instance_from_prior_medians():
-    model = af.Model(
-        af.Gaussian
-    )
-    db.Object.from_object(
-        model
-    )()
-    db.Object.from_object(
-        af.Gaussian()
-    )()
+@pytest.fixture(name="model")
+def make_model():
+    return af.Model(af.Gaussian)
+
+
+def test_instance_from_prior_medians(model):
+    db.Object.from_object(model)()
+    db.Object.from_object(af.Gaussian())()
     instance = model.instance_from_prior_medians()
-    db.Object.from_object(
-        instance
-    )()
+    db.Object.from_object(instance)()
+
+
+def test_object_to_instance(model):
+    assert isinstance(
+        db.Object.from_object(model)().instance_from_prior_medians(),
+        af.Gaussian,
+    )
