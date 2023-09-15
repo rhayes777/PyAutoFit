1bb857e1f914d4c64524c87cba64e45d4dddaa07
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-09-15 14:42:07 +0100

    remove tests which are too difficult to maintaing

--- test_autofit/non_linear/search/optimize/test_pyswarms.py
+++ test_autofit/non_linear/search/optimize/test_pyswarms.py
@@ -69,32 +69,4 @@ def test__loads_from_config_file_correct():
     assert search.number_of_cores == 1
 
 
-def test__samples_via_internal_from():
-    search = af.PySwarmsGlobal()
-    search.paths = af.DirectoryPaths(path_prefix=path.join("non_linear", "pyswarms"))
-    search.paths._identifier = "tag"
 
-    model = af.ModelMapper(mock_class=af.m.MockClassx3)
-    model.mock_class.one = af.LogUniformPrior(lower_limit=1e-8, upper_limit=100.0)
-    model.mock_class.two = af.LogUniformPrior(lower_limit=1e-8, upper_limit=100.0)
-    model.mock_class.three = af.LogUniformPrior(lower_limit=1e-8, upper_limit=100.0)
-    # model.mock_class.four = af.LogUniformPrior(lower_limit=1e-8, upper_limit=100.0)
-
-    samples = search.samples_via_internal_from(model=model)
-
-    assert isinstance(samples.parameter_lists, list)
-    assert isinstance(samples.parameter_lists[0], list)
-    assert isinstance(samples.log_likelihood_list, list)
-    assert isinstance(samples.log_prior_list, list)
-    assert isinstance(samples.log_posterior_list, list)
-
-    assert samples.parameter_lists[0] == pytest.approx(
-        [50.1254, 1.04626, 10.09456], 1.0e-4
-    )
-
-    assert samples.log_likelihood_list[0] == pytest.approx(-2780.995417544426, 1.0e-4)
-    assert samples.log_posterior_list[0] == pytest.approx(-2779.9206, 1.0e-4)
-    assert samples.weight_list[0] == 1.0
-
-    assert len(samples.parameter_lists) == 2
-    assert len(samples.log_likelihood_list) == 2
