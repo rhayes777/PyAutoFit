790c546e7a19dcbc1e0ae5a73d4295d94c90d486
Richard <richard@rghsoftware.co.uk>
2023-10-02 16:54:33 +0100

    use to_dict for attributes of a model object

--- test_autofit/serialise/test_regression.py
+++ test_autofit/serialise/test_regression.py
@@ -1,5 +1,6 @@
 import json
 
+from autoconf.dictable import to_dict, from_dict
 from autofit.mapper.mock.mock_model import MockClassx2Tuple
 import autofit as af
 
@@ -7,3 +8,15 @@ import autofit as af
 def test_tuple():
     model = af.Model(MockClassx2Tuple)
     json.dumps(model.dict())
+
+
+class AttributeNotInInit:
+    def __init__(self):
+        self.attribute = 1
+
+
+def test_embedded():
+    model = af.Collection(instance=AttributeNotInInit())
+
+    print(to_dict(model))
+    from_dict(to_dict(model))
