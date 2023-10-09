8be0501f7ea9c3959b90d88c2e896b50cf59fe08
Richard <richard@rghsoftware.co.uk>
2023-10-09 15:35:00 +0100

    fix for efficient samples

--- test_autofit/non_linear/samples/test_efficient.py
+++ test_autofit/non_linear/samples/test_efficient.py
@@ -16,7 +16,13 @@ def make_samples():
                 log_prior=2.0,
                 weight=4.0,
                 kwargs={"centre": 1.0, "normalization": 2.0, "sigma": 3.0},
-            )
+            ),
+            af.Sample(
+                log_likelihood=1.0,
+                log_prior=2.0,
+                weight=4.0,
+                kwargs={"centre": 4.0, "normalization": 5.0, "sigma": 6.0},
+            ),
         ],
     )
 
@@ -33,6 +39,20 @@ def test_conversion(efficient):
     assert sample.log_likelihood == 1.0
     assert sample.log_prior == 2.0
     assert sample.weight == 4.0
+
+
+def test_values(efficient):
+    samples = efficient.samples
+
+    sample_1, sample_2 = samples.sample_list
+
+    assert sample_1.kwargs["centre"] == 1.0
+    assert sample_1.kwargs["normalization"] == 2.0
+    assert sample_1.kwargs["sigma"] == 3.0
+
+    assert sample_2.kwargs["centre"] == 4.0
+    assert sample_2.kwargs["normalization"] == 5.0
+    assert sample_2.kwargs["sigma"] == 6.0
 
 
 def test_database(efficient, session):
