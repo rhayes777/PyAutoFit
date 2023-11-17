1b10e962da0fa57ff3ac040b3581243d82d33317
Richard <richard@rghsoftware.co.uk>
2023-11-17 14:01:31 +0000

    print message when lots of searches are aggregated form the directory

--- autofit/aggregator/aggregator.py
+++ autofit/aggregator/aggregator.py
@@ -115,6 +115,10 @@ class Aggregator:
         search_outputs
             A list of search_outputs
         """
+        if len(search_outputs) > 20:
+            print(
+                "When aggregating many searches it can be more efficient to use the database."
+            )
         self.search_outputs = search_outputs
         self.grid_search_outputs = grid_search_outputs
 
