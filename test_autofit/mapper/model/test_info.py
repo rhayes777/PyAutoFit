40070959d6464ea10ae66087ca39b725e822382e
Richard <richard@rghsoftware.co.uk>
2023-11-17 08:50:57 +0000

    put message in model info when no prior configuration is set

---
+++ test_autofit/mapper/model/test_info.py
@@ -0,0 +1,19 @@
+import autofit as af
+
+
+class Class:
+    def __init__(self, argument: float = 0.0):
+        self.argument = argument
+
+
+def test_placeholder():
+    model = af.Model(Class)
+
+    assert (
+        model.info
+        == """Total Free Parameters = 0
+
+
+
+argument                                                                        No prior configuration found."""
+    )
