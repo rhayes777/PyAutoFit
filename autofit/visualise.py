68b445379c6d8b2f730378d0178417485bbc3449
Richard <richard@rghsoftware.co.uk>
2024-02-05 14:28:17 +0000

    id fixes

--- autofit/visualise.py
+++ autofit/visualise.py
@@ -4,6 +4,7 @@ from pyvis.network import Network
 from autofit import (
     AbstractPriorModel,
     Model,
+    Collection,
     UniformPrior,
     GaussianPrior,
     LogGaussianPrior,
@@ -12,16 +13,18 @@ from autofit import (
 
 
 def str_for_object(obj):
+    if isinstance(obj, Collection):
+        return f"{obj.id}:Collection({len(obj)})"
     if isinstance(obj, Model):
-        return f"Model({obj.cls.__name__})"
+        return f"{obj.id}:Model({obj.cls.__name__})"
     if isinstance(obj, UniformPrior):
-        return f"UniformPrior_{obj.id}({obj.lower_limit}, {obj.upper_limit})"
+        return f"{obj.id}:UniformPrior({obj.lower_limit}, {obj.upper_limit})"
     if isinstance(obj, GaussianPrior):
-        return f"GaussianPrior_{obj.id}({obj.mean}, {obj.sigma})"
+        return f"{obj.id}:GaussianPrior({obj.mean}, {obj.sigma})"
     if isinstance(obj, LogGaussianPrior):
-        return f"LogGaussianPrior_{obj.id}({obj.mean}, {obj.sigma})"
+        return f"{obj.id}:LogGaussianPrior({obj.mean}, {obj.sigma})"
     if isinstance(obj, LogUniformPrior):
-        return f"LogUniformPrior_{obj.id}({obj.lower_limit}, {obj.upper_limit})"
+        return f"{obj.id}:LogUniformPrior({obj.lower_limit}, {obj.upper_limit})"
 
     return repr(obj)
 
@@ -33,17 +36,28 @@ class VisualiseGraph:
     def graph(self):
         graph = nx.Graph()
 
-        model_name = str_for_object(self.model)
-        graph.add_node(model_name)
+        def add_model(model):
+            model_name = str_for_object(model)
+            graph.add_node(model_name)
 
-        for name, prior in self.model.direct_prior_tuples:
-            prior_name = str_for_object(prior)
-            graph.add_node(prior_name)
-            graph.add_edge(
-                model_name,
-                prior_name,
-                label=name,
-            )
+            for name, prior in model.direct_prior_tuples:
+                prior_name = str_for_object(prior)
+                graph.add_node(prior_name)
+                graph.add_edge(
+                    model_name,
+                    prior_name,
+                    label=name,
+                )
+
+            for name, child_model in model.direct_prior_model_tuples:
+                add_model(child_model)
+                graph.add_edge(
+                    model_name,
+                    str_for_object(child_model),
+                    label=name,
+                )
+
+        add_model(self.model)
 
         # # Add nodes
         # G.add_node("A")
--- test_autofit/test_visualise.py
+++ test_autofit/test_visualise.py
@@ -9,12 +9,16 @@ from autofit.visualise import VisualiseGraph
 
 @pytest.fixture(autouse=True)
 def reset_ids():
+    af.ModelObject._ids = itertools.count()
     af.Prior._ids = itertools.count()
 
 
 @pytest.fixture
 def model():
-    return af.Model(af.Gaussian)
+    return af.Collection(
+        first=af.Model(af.Gaussian),
+        second=af.Model(af.Gaussian),
+    )
 
 
 @pytest.fixture
@@ -34,11 +38,19 @@ def graph(visualise_graph):
     return visualise_graph.graph()
 
 
-@pytest.mark.parametrize("node", ["Model(Gaussian)", "UniformPrior_0(0.0, 1.0)"])
+@pytest.mark.parametrize("node", ["0:Model(Gaussian)", "3:UniformPrior(0.0, 1.0)"])
 def test_nodes(graph, node):
     assert node in graph.nodes
 
 
-@pytest.mark.parametrize("edge", [("Model(Gaussian)", "UniformPrior_0(0.0, 1.0)")])
+@pytest.mark.parametrize(
+    "edge",
+    [
+        ("2:Collection(2)", "0:Model(Gaussian)"),
+        ("2:Collection(2)", "1:Model(Gaussian)"),
+        ("0:Model(Gaussian)", "0:UniformPrior(0.0, 1.0)"),
+        ("0:Model(Gaussian)", "1:UniformPrior(0.0, 1.0)"),
+    ],
+)
 def test_edges(graph, edge):
     assert edge in graph.edges
