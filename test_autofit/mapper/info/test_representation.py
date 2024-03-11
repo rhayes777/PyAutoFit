9a8b8f0f69bbd8cde2e52aa01b56e1548d063bdf
Richard <richard@rghsoftware.co.uk>
2024-03-11 16:44:26 +0000

    more tests

--- test_autofit/mapper/info/test_representation.py
+++ test_autofit/mapper/info/test_representation.py
@@ -38,3 +38,23 @@ def test_find_groups():
     info_paths = [((i, 1), 2) for i in range(3)]
     groups = find_groups(info_paths)
     assert groups == [(("0 - 2", 1), 2)]
+
+
+def test_two_groups():
+    info_paths = [((i, 1), 2) for i in range(3)] + [((i, 2), 2) for i in range(3)]
+    groups = find_groups(info_paths)
+    assert groups == [(("0 - 2", 1), 2), (("0 - 2", 2), 2)]
+
+
+def test_equal_prior_values():
+    info_paths = [((i, 1), af.UniformPrior(0.0, 1.0)) for i in range(3)]
+    groups = find_groups(info_paths)
+    assert len(groups) == 1
+    ((path, _),) = groups
+    assert path == ("0 - 2", 1)
+
+
+def test_different_priors():
+    info_paths = [((i, 1), af.UniformPrior(0.0, i + 1)) for i in range(3)]
+    groups = find_groups(info_paths)
+    assert len(groups) == 3
