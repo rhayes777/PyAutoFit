91613c6adc56bcd533db304c451a20e6b66328b8
Richard <richard@rghsoftware.co.uk>
2024-04-08 11:13:36 +0100

    test to ensure search.log can be switched off

--- test_autofit/non_linear/search/test_logging.py
+++ test_autofit/non_linear/search/test_logging.py
@@ -1,6 +1,10 @@
 import logging
+import shutil
+
+import pytest
 
 import autofit as af
+from autoconf.conf import with_config
 
 
 class Analysis(af.mock.MockAnalysis):
@@ -9,11 +13,26 @@ class Analysis(af.mock.MockAnalysis):
         return super().log_likelihood_function(instance)
 
 
-def test_logging():
-    search = af.mock.MockSearch("name", fit_fast=False)
-    analysis = Analysis()
-    model = af.Model(af.Gaussian)
-    search.paths.remove_files = False
-    search.fit(model, analysis)
-    assert search.paths.output_path.exists()
+@pytest.fixture(name="run_search")
+def make_run_search():
+    def run_search():
+        search = af.mock.MockSearch("name", fit_fast=False)
+        search.paths.remove_files = False
+        analysis = Analysis()
+        model = af.Model(af.Gaussian)
+        search.fit(model, analysis)
+        return search
+
+    return run_search
+
+
+def test_logging(run_search):
+    search = run_search()
     assert (search.paths.output_path / "search.log").exists()
+
+
+@with_config("output", "search_log", value=False)
+def test_logging_off(run_search, output_directory):
+    shutil.rmtree(output_directory)
+    search = run_search()
+    assert not (search.paths.output_path / "search.log").exists()
