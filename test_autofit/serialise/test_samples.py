bcdb3474382a3fabceb372b285f75f6bd3e29f2a
Richard <richard@rghsoftware.co.uk>
2024-06-10 15:41:56 +0100

    fix test

--- test_autofit/serialise/test_samples.py
+++ test_autofit/serialise/test_samples.py
@@ -52,17 +52,47 @@ def make_summary_dict():
         "type": "instance",
         "class_path": "autofit.non_linear.samples.summary.SamplesSummary",
         "arguments": {
-            "errors_at_sigma_1": {
+            "median_pdf_sample": {
+                "type": "instance",
+                "class_path": "autofit.non_linear.samples.sample.Sample",
+                "arguments": {
+                    "log_likelihood": 4.0,
+                    "log_prior": 5.0,
+                    "weight": 6.0,
+                    "kwargs": {
+                        "type": "dict",
+                        "arguments": {
+                            "centre": 2.0,
+                            "normalization": 4.0,
+                            "sigma": 6.0,
+                        },
+                    },
+                },
+            },
+            "log_evidence": None,
+            "values_at_sigma_3": {
                 "type": "list",
-                "values": [(2.0, 0.0), (3.0, 0.0), (4.0, 0.0)],
+                "values": [
+                    {"type": "tuple", "values": [0.0, 2.0]},
+                    {"type": "tuple", "values": [1.0, 4.0]},
+                    {"type": "tuple", "values": [2.0, 6.0]},
+                ],
+            },
+            "values_at_sigma_1": {
+                "type": "list",
+                "values": [
+                    {"type": "tuple", "values": [0.0, 2.0]},
+                    {"type": "tuple", "values": [1.0, 4.0]},
+                    {"type": "tuple", "values": [2.0, 6.0]},
+                ],
             },
             "errors_at_sigma_3": {
                 "type": "list",
-                "values": [(2.0, 0.0), (3.0, 0.0), (4.0, 0.0)],
-            },
-            "values_at_sigma_3": {
-                "type": "list",
-                "values": [(0.0, 2.0), (1.0, 4.0), (2.0, 6.0)],
+                "values": [
+                    {"type": "tuple", "values": [2.0, 0.0]},
+                    {"type": "tuple", "values": [3.0, 0.0]},
+                    {"type": "tuple", "values": [4.0, 0.0]},
+                ],
             },
             "max_log_likelihood_sample": {
                 "type": "instance",
@@ -81,27 +111,13 @@ def make_summary_dict():
                     },
                 },
             },
-            "values_at_sigma_1": {
+            "errors_at_sigma_1": {
                 "type": "list",
-                "values": [(0.0, 2.0), (1.0, 4.0), (2.0, 6.0)],
-            },
-            "log_evidence": None,
-            "median_pdf_sample": {
-                "type": "instance",
-                "class_path": "autofit.non_linear.samples.sample.Sample",
-                "arguments": {
-                    "log_likelihood": 4.0,
-                    "log_prior": 5.0,
-                    "weight": 6.0,
-                    "kwargs": {
-                        "type": "dict",
-                        "arguments": {
-                            "centre": 2.0,
-                            "normalization": 4.0,
-                            "sigma": 6.0,
-                        },
-                    },
-                },
+                "values": [
+                    {"type": "tuple", "values": [2.0, 0.0]},
+                    {"type": "tuple", "values": [3.0, 0.0]},
+                    {"type": "tuple", "values": [4.0, 0.0]},
+                ],
             },
         },
     }
