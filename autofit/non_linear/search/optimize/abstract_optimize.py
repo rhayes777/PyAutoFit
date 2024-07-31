eca4f2067ad34bcd1d9c48b9f962f549351291d0
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-07-31 19:16:20 +0100

    move plotting of start point to AbstractOptimizer

--- autofit/non_linear/search/optimize/abstract_optimize.py
+++ autofit/non_linear/search/optimize/abstract_optimize.py
@@ -1,6 +1,9 @@
 from abc import ABC
+import copy
 
 from autoconf import conf
+from autofit.mapper.prior_model.abstract import AbstractPriorModel
+from autofit.non_linear.analysis import Analysis
 from autofit.non_linear.search.abstract_search import NonLinearSearch
 from autofit.non_linear.samples import Samples
 from autofit.non_linear.plot.optimize_plotters import OptimizePlotter
@@ -19,6 +22,46 @@ class AbstractOptimizer(NonLinearSearch, ABC):
     @property
     def plotter_cls(self):
         return OptimizePlotter
+
+    def plot_start_point(
+        self,
+        model: AbstractPriorModel,
+        analysis: Analysis,
+    ):
+        """
+        Visualize the starting point of the non-linear search, using an instance of the model at the starting point
+        of the maximum likelihood estimator.
+
+        Plots are output to a folder named `image_start` in the output path, so that the starting point model
+        can be compared to the final model inferred by the non-linear search.
+
+        Parameters
+        ----------
+        model
+            The model used by the non-linear search
+        analysis
+            The analysis which contains the visualization methods which plot the starting point model.
+
+        Returns
+        -------
+
+        """
+
+        self.logger.info(
+            f"Visualizing Starting Point Model in image_start folder."
+        )
+
+        instance = model.instance_from_vector(vector=x0)
+        paths = copy.copy(self.paths)
+        paths.image_path_suffix = "_start"
+
+        self.perform_visualization(
+            model=model,
+            analysis=analysis,
+            instance=instance,
+            during_analysis=False,
+            paths_override=paths,
+        )
 
     def plot_results(self, samples):
 
