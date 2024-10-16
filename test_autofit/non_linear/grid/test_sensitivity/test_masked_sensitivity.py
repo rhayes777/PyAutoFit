bb8e4cc8e88b4160aadaff5a4d24a9918fa83e8a
Richard <richard@rghsoftware.co.uk>
2024-10-16 08:35:32 +0100

    fixture

--- test_autofit/non_linear/grid/test_sensitivity/test_masked_sensitivity.py
+++ test_autofit/non_linear/grid/test_sensitivity/test_masked_sensitivity.py
@@ -1,7 +1,13 @@
 from math import prod
 
+import pytest
 
-def test_run(masked_sensitivity):
-    result = masked_sensitivity.run()
+
+@pytest.fixture(name="masked_result")
+def make_masked_result(masked_sensitivity):
+    return masked_sensitivity.run()
+
+
+def test_run(masked_sensitivity, masked_result):
     number_elements = prod(masked_sensitivity.shape)
-    assert len(result.samples) == number_elements
+    assert len(masked_result.samples) == number_elements
