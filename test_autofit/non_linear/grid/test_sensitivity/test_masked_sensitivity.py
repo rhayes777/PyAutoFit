1e6652adb4dce82d07965cdb222326735bf2409a
Richard <richard@rghsoftware.co.uk>
2024-10-16 15:34:40 +0100

    with_limits implementation for uniform prior

--- test_autofit/non_linear/grid/test_sensitivity/test_masked_sensitivity.py
+++ test_autofit/non_linear/grid/test_sensitivity/test_masked_sensitivity.py
@@ -2,6 +2,8 @@ from math import prod
 
 import pytest
 
+import autofit as af
+
 
 @pytest.fixture(name="masked_result")
 def make_masked_result(masked_sensitivity):
@@ -19,3 +21,30 @@ def test_sample(masked_result):
     assert sample.model.perturb is not None
     assert sample.log_evidence == 0.0
     assert sample.log_likelihood == 0.0
+
+
+@pytest.mark.parametrize(
+    "lower, upper, mean",
+    [
+        (0.0, 1.0, 0.5),
+        (-1.0, 1.0, 0.0),
+        (-1.0, 0.0, -0.5),
+        (0.5, 1.0, 0.75),
+    ],
+)
+def test_mean_uniform_prior(
+    lower,
+    upper,
+    mean,
+):
+    prior = af.UniformPrior(
+        lower_limit=0.0,
+        upper_limit=1.0,
+    )
+    assert (
+        prior.with_limits(
+            lower,
+            upper,
+        ).mean
+        == mean
+    )
