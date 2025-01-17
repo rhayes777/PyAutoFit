b42814273e79a7d3134e8adb339e1419adff5347
Richard <richard@rghsoftware.co.uk>
2025-01-17 12:36:21 +0000

    docs

--- autofit/aggregator/aggregate_summary.py
+++ autofit/aggregator/aggregate_summary.py
@@ -1,6 +1,9 @@
-from typing import Optional, Callable
+from typing import Optional, Callable, List, Union
 
-from autofit.aggregator import Aggregator
+from pathlib import Path
+
+from autofit.aggregator.aggregator import Aggregator
+from autofit.aggregator.search_output import SearchOutput
 
 import csv
 
@@ -56,13 +59,33 @@ class ComputedColumn:
 
 
 class Row:
-    def __init__(self, result, columns, computed_columns):
+    def __init__(
+        self,
+        result: SearchOutput,
+        columns: List[Column],
+        computed_columns: List[ComputedColumn],
+    ):
+        """
+        A row in the summary table corresponding to one search.
+
+        Parameters
+        ----------
+        result
+            The search output from the aggregator
+        columns
+            The columns to include in the summary. These are taken from samples_summary or latent_summary
+        computed_columns
+            Columns filled with values computed from the samples
+        """
         self._result = result
         self._columns = columns
         self._computed_columns = computed_columns
 
     @property
-    def kwargs(self):
+    def kwargs(self) -> dict:
+        """
+        The median_pdf_sample arguments for the search from the samples_summary and latent_summary.
+        """
         samples_summary = self._result.value("samples_summary")
         kwargs = samples_summary.median_pdf_sample.kwargs
 
@@ -72,7 +95,10 @@ class Row:
 
         return kwargs
 
-    def dict(self):
+    def dict(self) -> dict:
+        """
+        The row as a dictionary including an id and one entry for each column.
+        """
         row = {"id": self._result.id}
         for column in self._columns:
             value = self.kwargs[column.path]
@@ -108,6 +134,19 @@ class AggregateSummary:
         argument: str,
         name: Optional[str] = None,
     ):
+        """
+        Add a column to the summary table.
+
+        This will be taken from the samples_summary or latent_summary.
+
+        Parameters
+        ----------
+        argument
+            The argument as it appears in the median_pdf_sample arguments
+            e.g. "galaxies.lens.bulge.centre.centre_0"
+        name
+            An optional name for the column. If not provided, the argument will be used.
+        """
         self._columns.append(
             Column(
                 argument,
@@ -115,15 +154,48 @@ class AggregateSummary:
             )
         )
 
+    def add_computed_column(
+        self,
+        name: str,
+        compute: Callable,
+    ):
+        """
+        Add a column to the summary table that is computed from other columns.
+
+        Parameters
+        ----------
+        name
+            The name of the column
+        compute
+            A function that takes the median_pdf_sample arguments and returns the computed value
+        """
+        self._computed_columns.append(
+            ComputedColumn(
+                name,
+                compute,
+            )
+        )
+
     @property
-    def fieldnames(self):
+    def fieldnames(self) -> List[str]:
+        """
+        The fieldnames for the CSV file.
+        """
         return (
             ["id"]
             + [column.name for column in self._columns]
             + [column.name for column in self._computed_columns]
         )
 
-    def save(self, path):
+    def save(self, path: Union[str, Path]):
+        """
+        Save the summary table to a CSV file.
+
+        Parameters
+        ----------
+        path
+            The path to save the file to
+        """
         with open(path, "w") as f:
             writer = csv.DictWriter(
                 f,
@@ -138,15 +210,3 @@ class AggregateSummary:
                 )
 
                 writer.writerow(row.dict())
-
-    def add_computed_column(
-        self,
-        name: str,
-        compute: Callable,
-    ):
-        self._computed_columns.append(
-            ComputedColumn(
-                name,
-                compute,
-            )
-        )
