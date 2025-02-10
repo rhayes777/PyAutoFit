fb23bde80ba1f224925fe5bba1404aa2ef40d3ce
James Nightingale <james.w.nightingale@durham.ac.uk>
2025-02-10 19:14:53 +0000

    undo sensitivity

--- autofit/non_linear/grid/sensitivity/__init__.py
+++ autofit/non_linear/grid/sensitivity/__init__.py
@@ -156,8 +156,6 @@ class Sensitivity:
                 )
             )
 
-            print(number)
-
             if not self._should_bypass(number=number):
                 jobs.append(self._make_job(number))
 
@@ -375,7 +373,6 @@ class Sensitivity:
 
     def _should_bypass(self, number: int) -> bool:
         shape_index = self.shape_index_from_number(number=number)
-        print(shape_index)
         return self.mask is not None and np.asarray(self.mask)[shape_index]
 
     def _make_jobs(self) -> Generator[Job, None, None]:
