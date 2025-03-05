dd821486a71cedce4447a4674f26e0f36b6870a7
Richard <richard@rghsoftware.co.uk>
2025-03-05 10:10:36 +0000

    basic summarisation of fits

--- autofit/aggregator/summary/aggregate_fits.py
+++ autofit/aggregator/summary/aggregate_fits.py
@@ -1,10 +1,33 @@
+from enum import Enum
+
+from astropy.io import fits
+
 from autofit.aggregator import Aggregator
+from autofit.aggregator.summary.subplot_filename import subplot_filename
+
+
+class Fit(Enum):
+    ModelImage = "MODEL_IMAGE"
+    ResidualMap = "RESIDUAL_MAP"
+    NormalizedResidualMap = "NORMALIZED_RESIDUAL_MAP"
+    ChiSquaredMap = "CHI_SQUARED_MAP"
 
 
 class AggregateFITS:
     def __init__(self, aggregator: Aggregator):
         self.aggregator = aggregator
 
-    def extract_fits(self):
+    def extract_fits(self, *hdus):
+        output = [fits.PrimaryHDU()]
         for result in self.aggregator:
-            print(result.fits)
+            for hdu in hdus:
+                source = result.value(subplot_filename(hdu))
+                source_hdu = source[source.index_of(hdu.value)]
+                output.append(
+                    fits.ImageHDU(
+                        data=source_hdu.data,
+                        header=source_hdu.header,
+                    )
+                )
+
+        return fits.HDUList(output)
--- test_autofit/aggregator/summary_files/test_aggregate_fits.py
+++ test_autofit/aggregator/summary_files/test_aggregate_fits.py
@@ -1,6 +1,11 @@
 import autofit as af
+from autofit.aggregator.summary.aggregate_fits import Fit
 
 
 def test_aggregate(aggregator):
     summary = af.AggregateFITS(aggregator)
-    summary.extract_fits()
+    result = summary.extract_fits(
+        Fit.ModelImage,
+        Fit.ResidualMap,
+    )
+    assert len(result) == 5
